06bd327859e0a11709f82d4c996876f20282d1a2
ArnauDavis <arnau.davis@outlook.com>
2024-09-03 20:48:24 -0400

    added solved problems

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -24,3 +24,36 @@ function otherAngle(a, b) {
  function removeChar(str){
      return str.slice(1, -1)
    }
+
+
+
+//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. 
+//You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and 
+//weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your 
+//pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums 
+//all the rare candies you would need. 
+function candiesNeeded(){
+let bulbCandy = 11
+let catCandy = 6
+let weedCandy = 6
+totalCandies = bulbCandy + catCandy + weedCandy
+console.log(totalCandies)
+}
+
+//You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one 
+//function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+
+function convertCelToFah(fahrenheit){
+    celcius = (((fahrenheit - 32) * 5)/9)
+    return celcius
+}
+
+function canCharFight(temperature){
+    let convertedTemp = convertCelToFah(temperature)
+    
+    if(convertedTemp > 0 ){
+        console.log('fah says It\'s good')
+    } else {
+        console.log('gotta put a sweater on')
+    }
+}
