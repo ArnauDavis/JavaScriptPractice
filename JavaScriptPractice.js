647259519ee933ba5b72bb8f3c4f796d56eec6c6
ArnauDavis <arnau.davis@outlook.com>
2024-09-04 00:58:20 -0400

    I changed the paragraph from a // for every line to a /* */

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -27,11 +27,14 @@ function otherAngle(a, b) {
 
 
 
-//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. 
-//You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and 
-//weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your 
-//pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums 
-//all the rare candies you would need. 
+/* 
+You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. 
+You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and 
+weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your 
+pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums 
+all the rare candies you would need. 
+*/
+
 function candiesNeeded(){
 let bulbCandy = 11
 let catCandy = 6
@@ -39,6 +42,8 @@ let weedCandy = 6
 totalCandies = bulbCandy + catCandy + weedCandy
 console.log(totalCandies)
 }
+
+
 
 //You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one 
 //function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
