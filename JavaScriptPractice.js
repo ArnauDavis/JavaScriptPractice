264eff27b459c4db9d3ed150b7b58de4408c0180
ArnauDavis <arnau.davis@outlook.com>
2024-09-05 16:55:24 -0400

    added problem solved from 'thejsway'

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -75,6 +75,7 @@ function reverseTheTeam(){
 }
 
 
+
 //You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. 
 //Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party 
 
@@ -82,5 +83,37 @@ numberOfTrainers = 10
 for(let i = 1; i <= numberOfTrainers; i++){
     console.log('Pikachu I choose you')
 }
+
+
+
+// Dogs taller than 60 emote "Grrr! Grrr!" when they bark, other ones yip "Woof! Woof!"
+
+class Dog {
+  constructor(name, species, size){
+    this.name = name
+    this.species = species
+    this.size = size
+  }
+    bark() {
+      if(this.size > 60){
+        return "Grrr! Grrr!"
+      } else if (this.size <= 60){
+        return "Woof! Woof!"
+      }
+      else {
+        return "didn't work, try again :)"
+      }
+    }
+  
+}
+
+const fang = new Dog("Fang", "boarhound", 75)
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`)
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`)
+
+const snowy = new Dog("Snowy", "terrier", 22)
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`)
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`)
+
 
 
