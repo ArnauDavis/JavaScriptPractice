6f7a410c2c54dd60c85b0d30fd52cdc705b82854
ArnauDavis <arnau.davis@outlook.com>
2024-09-09 13:52:18 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -172,5 +172,13 @@ function sortEmAddEm (arr){
 
 
 
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+function square(sample){
+  return Math.pow(sample,2)  
+}
+
+
 
 
