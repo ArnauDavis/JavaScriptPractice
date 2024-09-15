507e72fb7fb7f0cd75d76801ce7121278765d0a0
ArnauDavis <arnau.davis@outlook.com>
2024-09-14 23:05:47 -0400

    added new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -249,6 +249,21 @@ function paperwork(n, m) {
 }
 
 
+//Can you find the needle in the haystack?
+
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+//After your function finds the needle it should return a message (as a string) that says:
+
+//"found the needle at position " plus the index it found the needle, so: 
+
+
+const haystack = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]
+function findNeedle(haystack) {
+    console.log(`found the needle at position ${haystack.indexOf("needle")}`)
+  }
+
+
 
 
 
