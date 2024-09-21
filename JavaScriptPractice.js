21c29fa1af6c3a5c0b594d20612753ef365bccc5
ArnauDavis <arnau.davis@outlook.com>
2024-09-20 23:10:44 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -307,6 +307,14 @@ function solution(str, ending){
   }
 }
 
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+function squareSum(arrayah){
+  const finalAnswer = arrayah.reduce((a,c) => a + Math.pow(c,2), 0,)
+  return finalAnswer
+}
+
+
 
 
 
