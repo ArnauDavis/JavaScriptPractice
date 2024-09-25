87afce7ae03d74384d930106941996770881f518
ArnauDavis <arnau.davis@outlook.com>
2024-09-24 23:47:43 -0400

    added new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -351,6 +351,16 @@ function boolToWord( bool ){
 }
 
 
+//Write a program that finds the summation of every number from 1 to num. 
+//The number will always be a positive integer greater than 0. Your function only needs to return the result.
+
+function summation (num) {
+  let sum = 0
+  for(i=1; i<=num; i++){
+  sum += i
+  }
+  return sum
+}
 
 
 
