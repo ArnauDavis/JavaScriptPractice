80564143c001ea6fd04e82de0f29bde5ff139cad
ArnauDavis <arnau.davis@outlook.com>
2024-09-28 19:25:30 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -411,5 +411,10 @@ let Alex = new Fighter(100,'Special Tornado','skill issue huh','you should pract
 
 
 
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number*8 : number*9
+}
 
 
