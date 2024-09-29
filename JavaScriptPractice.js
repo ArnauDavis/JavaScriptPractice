122e2104d5c6af23fbb7f16a9cfead6b4e8c651a
ArnauDavis <arnau.davis@outlook.com>
2024-09-29 19:41:34 -0400

    added new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -418,3 +418,28 @@ function simpleMultiplication(number) {
 }
 
 
+
+/*Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, 
+
+neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+function disemvowel(str) {
+  let sentence = str.split('')
+  sentence = sentence.filter((letter) => 
+  letter !== 'a' && letter !== 'e' && letter !== 'i' &&
+  letter !== 'o' && letter !== 'u' && letter !=='A' && 
+  letter !== 'E' && letter !== 'I' &&
+  letter !== 'O' && letter !== 'U'
+)
+  str = sentence.join('')
+  return str;
+}
