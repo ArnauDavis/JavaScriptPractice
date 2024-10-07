1e62cb2078d9f0098f6c5883ca52289bfc9fe075
ArnauDavis <arnau.davis@outlook.com>
2024-10-06 21:33:35 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -568,3 +568,18 @@ function getGrade (s1, s2, s3) {
   }
 }
 
+
+
+/*
+  Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+*/
+
+
+function problem(x){
+  if(typeof x == 'number'){
+    return x * 50 + 6
+  } else{
+    return 'Error'
+  }
+}
+
