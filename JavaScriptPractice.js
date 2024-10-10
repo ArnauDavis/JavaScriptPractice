a47222ab11225ec817fa1d94f9a3d74ccfdc26be
ArnauDavis <arnau.davis@outlook.com>
2024-10-09 22:42:25 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -617,3 +617,19 @@ function numberToString(num) {
   return num.toString()
 }
 
+
+
+/*
+ Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. 
+ If the array does not contain any numbers then you should return 0.
+*/
+
+function sum (numbers) {
+  if(sum.length == 0){
+    return 0
+  } else{
+    return numbers.reduce((ac,curr)=> ac + curr,0)
+  }
+  
+}
+
