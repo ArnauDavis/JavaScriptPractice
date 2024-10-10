33920c2958dc6652e2a9208c3c6f5785ee350149
ArnauDavis <arnau.davis@outlook.com>
2024-10-10 03:07:59 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -633,3 +633,19 @@ function sum (numbers) {
   
 }
 
+
+
+/*
+ Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+ If no occurrences can be found, a count of 0 should be returned.
+*/
+
+function strCount(str, letter){
+  let lowStr = str.toLowerCase()  
+  let strAr = lowStr.split('')
+  let filtered = strAr.filter((bits) => bits == letter)
+  return filtered.length
+}
+
+
