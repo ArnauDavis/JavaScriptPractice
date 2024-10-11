e0a69fb7cc4f21b30af71576849a6f54db443932
ArnauDavis <arnau.davis@outlook.com>
2024-10-11 00:36:32 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -649,3 +649,22 @@ function strCount(str, letter){
 }
 
 
+
+/*
+ You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+function check(a, x) {
+  let checked = a.filter((elem) => elem == x)
+  if (checked.length >= 1){
+    return true
+  } else{
+    return false
+  }
+}
+
+
