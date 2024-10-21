f0b64becb5fe22940e8c74e69d1799c280d91eb5
ArnauDavis <arnau.davis@outlook.com>
2024-10-20 23:44:09 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -770,3 +770,13 @@ var isSquare = function(n){
 }
 
 
+
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+  let words = s.split(' ')
+  let minLength = words[0].length
+  words.forEach((word)=> {if (word.length < minLength){minLength = word.length}})
+  return minLength
+}
