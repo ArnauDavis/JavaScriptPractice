0c1459021c43595b6bbf7e771de468ad86c2d5f7
ArnauDavis <arnau.davis@outlook.com>
2024-10-23 22:05:23 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -828,3 +828,28 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals
 }
 
+
+// Consider an array/list of sheep where some sheep may be missing from their place. 
+// We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+
+// The correct answer would be 17.
+
+
+function countSheeps(sheep) {
+  let present = 0
+  sheep.forEach((sheepa) => {
+    if(sheepa == true){
+      present += 1
+    }
+  })
+  return present
+}
