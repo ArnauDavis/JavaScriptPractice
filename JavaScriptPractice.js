57f904431d0341d73cde3dd38911ba02bf65bfc0
ArnauDavis <arnau.davis@outlook.com>
2024-11-01 20:03:43 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -983,3 +983,30 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   }
 
 //I solved this one two ways just because it was fun
+
+
+
+  // All of the animals are having a feast! Each animal is bringing one dish. There is just one 
+  // rule: the dish must start and end with the same letters as the animal's name. For example, the 
+  // great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+  // Write a function feast that takes the animal's name and dish as arguments and returns true or false to 
+  // indicate whether the beast is allowed to bring the dish to the feast.
+  
+  // Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast 
+  // and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. 
+  // They will not contain numerals.
+  
+  function feast(beast, dish) {
+    const bCS = beast.split('')[0]
+    const dCS = dish.split('')[0]
+    const bCE = beast.split('')[beast.length - 1]
+    const dCE = dish.split('')[dish.length - 1]
+    
+    if(bCS == dCS && bCE == dCE){
+      return true
+    } else {
+      return false
+    }
+ //  I later learn I did not need to split
+    }
