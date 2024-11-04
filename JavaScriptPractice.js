a48f8c6396c8f73c4c84c540a6269c700b70959d
ArnauDavis <arnau.davis@outlook.com>
2024-11-03 20:11:08 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1010,3 +1010,25 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     }
  //  I later learn I did not need to split
     }
+
+
+
+//   There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+// Note:
+
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+
+function betterThanAverage(classPoints, yourPoints) {
+  if (yourPoints > classPoints.reduce((acc,curr)=>acc+curr,0)/classPoints.length){
+    return true
+  } else {
+    return false
+  }
+}
