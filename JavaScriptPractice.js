58b8cf0925778780e66e69d2fc91d1244f3600d7
ArnauDavis <arnau.davis@outlook.com>
2024-11-13 22:24:57 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1175,3 +1175,20 @@ function nameShuffler(str){
 function saleHotdogs(n){
   return n*(n < 5 ? 100 : n < 10 ? 95 : 90)
   }
+
+
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+  let ln = name.toLowerCase()
+  return ln.startsWith('r') ? `${name} plays banjo` : `${name} does not play banjo`
+}
