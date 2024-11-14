75914ef58549c862006b558e72bc80d7e51163b5
ArnauDavis <arnau.davis@outlook.com>
2024-11-14 08:51:01 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1192,3 +1192,42 @@ function areYouPlayingBanjo(name) {
   let ln = name.toLowerCase()
   return ln.startsWith('r') ? `${name} plays banjo` : `${name} does not play banjo`
 }
+
+
+
+/*
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+    Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+    A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+    Every smiling face must have a smiling mouth that should be marked with either ) or D
+
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+Example
+
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+Note
+
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+*/
+
+function countSmileys(arr){
+  let answer = 0
+    arr.forEach((face) => {
+      if((face.startsWith(':') || face.startsWith(';')) &&
+          (face.endsWith('D') || face.endsWith(')')) &&
+          (face.length === 2 || (face.length === 3 && (face[1] === '-' || face[1] === '~')))){
+         answer += 1
+         }
+    })
+  return answer
+}
