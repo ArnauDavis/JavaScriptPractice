34ef28d8e9d2c9cd97f08cb5e38cd2e63cbc3f87
ArnauDavis <arnau.davis@outlook.com>
2024-11-18 19:27:00 -0500

    added a new solved problem
    solved it two ways

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1307,3 +1307,41 @@ function remove (string) {
 function isAnagram (test, original) {
 	return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
 }
+
+
+
+/*
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+[sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+   7      6      5      4      3            2      1
+
+If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! 
+You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+Note: there will always be exactly one wolf in the array.
+Examples
+
+Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+Input: ["sheep", "sheep", "wolf"]
+Output: "Pls go away and stop eating my sheep"
+*/
+
+
+function warnTheSheep(queue) {
+  /*
+  let cryWolf = queue.length - queue.indexOf('wolf') -1
+  if(cryWolf > 0){
+    return `Oi! Sheep number ${cryWolf}! You are about to be eaten by a wolf!`
+  }else {
+    return `Pls go away and stop eating my sheep`
+  }
+  */
+  let cryWolf = queue.length - queue.indexOf('wolf') -1
+  return cryWolf > 0 ? `Oi! Sheep number ${cryWolf}! You are about to be eaten by a wolf!` : `Pls go away and stop eating my sheep`
+  
+}
