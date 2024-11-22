d9a096db4dc087a08686faf84077d8c2b2bcf386
ArnauDavis <arnau.davis@outlook.com>
2024-11-22 15:28:36 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1398,3 +1398,24 @@ function sumStr(a,b) {
 }
 
 
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+// Examples ( Input --> Output )
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1  #  because 114 is not a perfect square
+
+function findNextSquare(sq) {
+    let next = Math.sqrt(sq)
+    if(next%1 == 0){
+      return Math.pow((next += 1),2)
+    }else{
+      return -1;
+    }
+}
+
