b7c3c7a2bb63d77c95674af496c317957f39da2f
ArnauDavis <arnau.davis@outlook.com>
2024-11-27 22:54:15 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1525,3 +1525,11 @@ function isValidWalk(walk) {
   }
 }
 
+
+
+// Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+  return health >= damage ? health -= damage : health = 0
+}
+
