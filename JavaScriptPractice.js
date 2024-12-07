438b0c1ac9735542c270a37fe915fc673d6558e5
ArnauDavis <arnau.davis@outlook.com>
2024-12-06 22:49:56 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1609,3 +1609,30 @@ function findSmallestInt(arr) {
 // // If the result is positive (i.e., a > b), it tells JavaScript that b should come before a.
 // // If the result is zero (i.e., a == b), the order of a and b doesn't change.
 // //[0] is just to pick the first number in the array which should be the smallest.
+
+
+
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+//     make as few changes as possible.
+//     if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+
+// For example:
+
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+
+function solve(s){
+  let upper = 0
+  let lower = 0
+  s.split('').forEach((str)=>{
+    if(str == str.toLowerCase()){
+      lower += 1
+    }else if(str == str.toUpperCase()){
+      upper += 1
+    }
+  })
+return lower >= upper ? s.toLowerCase() : s.toUpperCase()
+}
