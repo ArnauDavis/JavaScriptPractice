290e3508240e273d14f6b4f3cca3ae304fe2d018
ArnauDavis <arnau.davis@outlook.com>
2024-12-07 23:59:26 -0500

    added new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1636,3 +1636,33 @@ function solve(s){
   })
 return lower >= upper ? s.toLowerCase() : s.toUpperCase()
 }
+
+
+
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an 
+// application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; 
+// the better the player the lower the handicap.
+// Input
+
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for 
+// the person's age and an integer for the person's handicap.
+
+function openOrSenior(data){
+  let check = []
+  data.forEach(([age,hand])=>{
+    if(age > 54 && hand > 7){
+      check.push('Senior')
+    }else{
+      check.push('Open')
+    }
+  })
+  return check
+}
+
+// //or 
+
+function openOrSeniorAgain(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
