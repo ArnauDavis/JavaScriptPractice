a671e336ad726de6efbfdd073d8fc71ea2cceabc
ArnauDavis <arnau.davis@outlook.com>
2024-12-09 18:11:47 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1688,3 +1688,22 @@ function minMax(arr){
   return [sorted[0],sorted[sorted.length-1]]
 }
 
+
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+function between(a, b) {
+  let answer = []
+  for(let i = a; i <= b; i++){
+    answer.push(i)
+  }
+ return answer 
+}
+
+
