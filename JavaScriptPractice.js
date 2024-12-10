8236ee965f1f2191d9b4ad87414d83b28e2bff05
ArnauDavis <arnau.davis@outlook.com>
2024-12-10 11:55:49 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1707,3 +1707,11 @@ function between(a, b) {
 }
 
 
+
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s.split('').filter((sign)=> sign != '!').join('')
+}
+
+
