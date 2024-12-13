4a4c001e3d76ddccf8e1ec4aa16a290a8a6ddad8
ArnauDavis <arnau.davis@outlook.com>
2024-12-13 15:48:05 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1771,3 +1771,19 @@ var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
 
 
 
+// Task
+
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example
+
+//     [1, -4, 7, 12] => 1+7+12=20 1 + 7 + 12 = 20 1+7+12=20
+
+// Note
+
+// If there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  let filtered = arr.filter((number)=>number >= 1)
+  let answer = filtered.reduce((acc,cur)=> acc + cur,0)
+  return answer
+}
