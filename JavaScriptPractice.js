8a7d3a1cbf1161b4d4b9a241f801cb301deed586
ArnauDavis <arnau.davis@outlook.com>
2024-12-14 22:50:06 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1787,3 +1787,24 @@ function positiveSum(arr) {
   let answer = filtered.reduce((acc,cur)=> acc + cur,0)
   return answer
 }
+
+
+
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) 
+// that receive a list of integers as input, and return the largest and lowest number in that list, respectively. Each function returns one number.
+// Examples (Input -> Output)
+
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+
+function min(arr){
+  arr.sort((a, b) => a - b)
+  return arr[0]
+}
+
+function max(arr){
+  arr.sort((a, b) => a - b)
+  return arr[arr.length - 1]
+}
