234ed2ee0def18b9836f437a11b4c1e08e072d61
ArnauDavis <arnau.davis@outlook.com>
2024-12-21 01:29:12 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1960,3 +1960,19 @@ function litres(time) {
   return Math.floor(Math.floor(time)*0.5);
 }
 
+
+
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or 
+// more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+// Example:
+
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+function sayHello( name, city, state ) {
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
+
+
