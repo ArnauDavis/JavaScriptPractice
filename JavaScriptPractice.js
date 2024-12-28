3c1760feac699ef274b8ac2f62b4ddc8f71d1a62
ArnauDavis <arnau.davis@outlook.com>
2024-12-27 23:18:00 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2074,3 +2074,23 @@ function hero(bullets, dragons){
   return bullets / 2 >= dragons ? true : false
 }
 
+
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number=function(array){
+  let answer = array.map((x,index)=>{
+    return `${index+1}: ${x}`
+  })
+  return answer
+}
+
