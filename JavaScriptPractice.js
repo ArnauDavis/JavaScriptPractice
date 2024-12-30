ecdcb919c7049157116d9c0e5b0481a44a5ac377
ArnauDavis <arnau.davis@outlook.com>
2024-12-29 22:32:52 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2117,3 +2117,19 @@ const rps = (p1, p2) => {
 }
 
 
+
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+//Examples (input -> output)
+
+//6, "I"     -> "IIIIII"
+//5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  let check =[]
+  for(let i=0; i<n; i++){
+  check.push(s)
+}
+  return check.join('')
+}
+
