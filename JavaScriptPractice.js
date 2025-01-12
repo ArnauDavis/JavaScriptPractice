5f9ba04b8ceb41cf1c15dfac21d110fb9b2636b9
ArnauDavis <arnau.davis@outlook.com>
2025-01-11 23:39:29 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2396,6 +2396,17 @@ function mouthSize(animal) {
 
 
 
+// Bob needs a fast way to calculate the volume of a cuboid with three values: 
+// the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
+
+
+
 
 
 
