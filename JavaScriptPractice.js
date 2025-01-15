b923d9a372fd5a9d03cd1a969e031934111c2eca
ArnauDavis <arnau.davis@outlook.com>
2025-01-14 22:51:59 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2452,3 +2452,23 @@ function enough(cap, on, wait) {
 }
 
 
+
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will 
+// consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference 
+// of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. 
+// Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+function findDifference(a, b) {
+  let part1 = a.reduce((acc,cur)=>acc*cur,1)
+  let part2 = b.reduce((acc,cur)=>acc*cur,1)
+  return part1>part2 ? part1-part2 : part2-part1
+}
+//or
+function findDifference2(a, b) {
+  return Math.abs(a.reduce((acc,cur)=> acc*cur, 1) - b.reduce((acc,cur)=>acc*cur,1))
+}
+
