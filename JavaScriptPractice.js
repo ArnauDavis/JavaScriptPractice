add756f14664f03a83ced30b22e1e4d9b1d8bf2f
ArnauDavis <arnau.davis@outlook.com>
2025-01-15 00:30:17 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2472,3 +2472,11 @@ function findDifference2(a, b) {
   return Math.abs(a.reduce((acc,cur)=> acc*cur, 1) - b.reduce((acc,cur)=>acc*cur,1))
 }
 
+
+
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+function angle(n) {
+  return (n-2)*180
+}
+
