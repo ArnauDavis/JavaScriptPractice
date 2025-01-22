3ed33a210f510a7bef2b65b88ecd832c8d2606b8
ArnauDavis <arnau.davis@outlook.com>
2025-01-21 23:55:52 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2561,3 +2561,30 @@ function rainAmount(mm){
     }
 }
 
+
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+// Example:
+
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+
+// Input constraints:
+
+//     0 <= h <= 23
+//     0 <= m <= 59
+//     0 <= s <= 59
+
+function past(h, m, s){
+  let total = 0
+  0 <= h <= 23 ? total += h*3600000 : total += 0
+  0 <= m <= 59 ? total += m*60000 : total += 0
+  0 <= s <= 59 ? total += s*1000 : total += 0
+  return total
+}
+
