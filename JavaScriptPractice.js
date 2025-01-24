24e8a01a5c81663b2db43ccac0d63f702174ce62
ArnauDavis <arnau.davis@outlook.com>
2025-01-23 22:54:25 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2628,3 +2628,22 @@ function domainName(url){
   return url.split('.')[0];
 }
 
+
+
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  let p1 = arr1.reduce((acc,curr)=>
+                      acc+curr,0)
+  let p2 = arr2.reduce(
+    (acc,curr)=> acc+curr,0)
+  return p1+p2
+}
+//or
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+
