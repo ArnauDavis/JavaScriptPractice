6cf79db2cce80557a734b2b6eb064d8fe5cad97e
ArnauDavis <arnau.davis@outlook.com>
2025-01-26 21:12:04 -0800

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2668,3 +2668,56 @@ throw undefined
 }
 
 
+
+// Those wayward Winchester boys are in trouble again, hunting something down in New Orleans. You are Bobby Singer, you know how "idjits" they can be, so you have to prepare. They will call 
+// you any minute with the race of the thing, and you want to answer as soon as possible. By answer, I mean: tell them how to kill, or fight it.
+
+// You have something like a database (more like drunken doodling) to help them:
+
+//     werewolf : Silver knife or bullet to the heart
+//     vampire : Behead it with a machete
+//     wendigo : Burn it to death
+//     shapeshifter : Silver knife or bullet to the heart
+//     angel : Use the angelic blade
+//     demon : Use Ruby's knife, or some Jesus-juice
+//     ghost : Salt and iron, and don't forget to burn the corpse
+//     dragon : You have to find the excalibur for that
+//     djinn : Stab it with silver knife dipped in a lamb's blood
+//     pagan god : It depends on which one it is
+//     leviathan : Use some Borax, then kill Dick
+//     ghoul : Behead it
+//     jefferson starship : Behead it with a silver blade
+//     reaper : If it's nasty, you should gank who controls it
+//     rugaru : Burn it alive
+//     skinwalker : A silver bullet will do it
+//     phoenix : Use the colt
+//     witch : They are humans
+//     else : I have friggin no idea yet
+
+function bob(mon) {
+  let db = {
+    'werewolf' : 'Silver knife or bullet to the heart, idjits!',
+    'vampire' : 'Behead it with a machete, idjits!',
+    'wendigo' : 'Burn it to death, idjits!',
+    'shapeshifter' : 'Silver knife or bullet to the heart, idjits!',
+    'angel' : 'Use the angelic blade, idjits!',
+    'demon' : 'Use Ruby\'s knife, or some Jesus-juice, idjits!',
+    'ghost' : 'Salt and iron, and don\'t forget to burn the corpse, idjits!',
+    'dragon' : 'You have to find the excalibur for that, idjits!',
+    'djinn' : 'Stab it with silver knife dipped in a lamb\'s blood, idjits!',
+    'pagan god' : 'It depends on which one it is, idjits!',
+    'leviathan' : 'Use some Borax, then kill Dick, idjits!',
+    'ghoul' : 'Behead it, idjits!',
+    'jefferson starship' : 'Behead it with a silver blade, idjits!',
+    'reaper' : 'If it\'s nasty, you should gank who controls it, idjits!',
+    'rugaru' : 'Burn it alive, idjits!',
+    'skinwalker' : 'A silver bullet will do it, idjits!',
+    'phoenix' : 'Use the colt, idjits!',
+    'witch' : 'They are humans, idjits!'
+  }
+  let other = 'I have friggin no idea yet, idjits!'
+  let answer = db[mon] || other
+  return answer
+}
+
+
