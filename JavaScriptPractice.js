2301200b59f013ef185c434cc94b6a9347951f9c
ArnauDavis <arnau.davis@outlook.com>
2025-02-05 21:44:43 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -2852,5 +2852,31 @@ function maskify(cc) {
 
 
 
+// Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed 
+// (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+// "This is a test        --> "This is a test" 
+// "This is another test" --> "This is rehtona test"
+
+function spinWords(string){
+  let p1 = string.split(' ')
+  let p22 = []
+  p1.forEach((x)=>{
+    if(x.length >= 5){
+      p22.push(x.split('').reverse().join(''))
+    }else{
+      p22.push(x)
+    }
+  })
+  let answer = p22.join(' ')
+  return answer
+}
+
+
+
+
 
 
