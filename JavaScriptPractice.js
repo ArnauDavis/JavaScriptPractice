ba144a7bd22576e80cb34b89a8ff046697b45bdb
ArnauDavis <arnau.davis@outlook.com>
2025-02-15 23:29:31 -0500

    added two new solved problems
    one extra to make up for not being able to connect to this repo yesterday

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3069,3 +3069,70 @@ function rowSumOddNumbers(n) {
 	return Math.pow(n,3)
 }
 
+
+	
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+  let ans = []
+  for(let i = n; i > 0; i--){
+    ans.push(i)
+  }
+  return ans
+}
+
+
+
+// There are five workers : James, John, Robert, Michael and William. They work in a fixed order from Monday to Friday, 
+// and they rest on weekends. The order of work is as follows:
+
+//     James works on Monday,
+
+//     John works on Tuesday,
+
+//     Robert works on Wednesday,
+
+//     Michael works on Thursday,
+
+//     William works on Friday.
+
+// Your task is to create a function that takes three arguments:
+
+//     Weekday: The day of the week (e.g., Monday).
+
+//     Number of Trees: The number of trees that must be sprayed on that day.
+
+//     Cost per Litre: The cost of 1 litre of liquid needed to spray a tree. Assume that 1 tree requires 1 litre of liquid.
+
+// The total cost of the liquid to spray all trees is denoted as x.
+
+// The function should return a string in the following format:
+
+// 'It is (weekday) today, (name), you have to work, you must spray (number) trees and you need (x) dollars to buy liquid'
+// Examples:
+
+// Input :
+
+//     Weekday : "Monday".
+
+//     Number of Trees : 15
+
+//     Cost per Litre : 2
+
+// Output:
+
+// "It is Monday today, James, you have to work, you must spray 15 trees and you need 30 dollars to buy liquid."
+
+function task(w, t, c) {
+  let dw = {
+    'Monday': 'James',
+    'Tuesday': 'John',
+    'Wednesday': 'Robert',
+    'Thursday': 'Michael',
+    'Friday': 'William'
+  }
+  return `It is ${w} today, ${dw[w]}, you have to work, you must spray ${t} trees and you need ${t*c} dollars to buy liquid` 
+}
+
