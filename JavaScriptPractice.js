aba759c612c19a4a550e88d851d8531ecef9308e
ArnauDavis <arnau.davis@outlook.com>
2025-02-16 10:03:04 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3136,3 +3136,16 @@ function task(w, t, c) {
   return `It is ${w} today, ${dw[w]}, you have to work, you must spray ${t} trees and you need ${t*c} dollars to buy liquid` 
 }
 
+
+
+// It's a well known fact that anything Chuck Norris wants, he gets. As a result Chuck very rarely has to use the word false.
+
+// It is a less well known fact that if something is true, and Chuck doesn't want it to be, Chuck can scare the truth with his massive biceps, and it automatically becomes false.
+
+// Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+// Go show some truth who's boss!
+
+function ifChuckSaysSo(){
+  return typeof('data') == Number
+}
