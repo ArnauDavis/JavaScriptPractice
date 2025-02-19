6e28fd7a68dabccd6166a69143790ede30921bd8
ArnauDavis <arnau.davis@outlook.com>
2025-02-18 23:04:07 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3207,3 +3207,30 @@ function isDivisible(n, x, y) {
 function isDivisible2(n, x, y) {
   return n%x==0? n%y==0 : false ? true : false
 }
+
+
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 
+// (November), is part of the fourth quarter.
+
+// Constraint:
+
+//     1 <= month <= 12
+
+const quarterOf = (month) => {
+  if(month <= 3){
+    return 1
+  }else if(month <= 6){
+    return 2
+  }else if(month <= 9){
+    return 3
+  }else{
+    return 4
+  }
+}
+//or
+const quarterOf2 = (month) => {
+  return month <= 3? 1 : month <= 6? 2 : month <= 9? 3 : 4
+}	
