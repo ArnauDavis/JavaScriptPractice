fcfc9e63b655d1ad4a2c6a4d1b2e53bdb486ba46
ArnauDavis <arnau.davis@outlook.com>
2025-02-23 23:53:30 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3271,3 +3271,23 @@ function hexStringToRGB(hexString) {
 function buildString(...template){
   return `I like ${template.join(', ')}!`;
 }
+
+
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+function removeEveryOther(arr){
+  let ans = [arr[0]]
+  for(let i = 1; i < arr.length; i++){
+    if(i % 2 == 0){
+      ans.push(arr[i])
+    }
+  }
+  return ans
+}
+	
