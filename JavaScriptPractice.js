b20c20d996875f89d049af882eb66d11c1ed8a60
ArnauDavis <arnau.davis@outlook.com>
2025-02-24 20:24:29 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3290,4 +3290,13 @@ function removeEveryOther(arr){
   }
   return ans
 }
-	
+
+
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth){
+  let area = 2 * width * height + 2 * width * depth + 2 * depth * height  
+  let volume = width * height * depth
+  return [area, volume]
+}	
