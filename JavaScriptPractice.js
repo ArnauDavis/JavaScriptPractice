68fdb1c14381127473ee8f39fca8ac3d15b9bba2
ArnauDavis <arnau.davis@outlook.com>
2025-03-09 23:01:49 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3510,4 +3510,37 @@ var isPalindrome = function(s) {
       return false
   }
 }	
+
+
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+
+// consisting of non-space characters only.
+
+ 
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+var lengthOfLastWord = function(s) {
+  let p1 = s.trim().split(' ')
+  return p1[p1.length-1].length
+}
+	
 	
