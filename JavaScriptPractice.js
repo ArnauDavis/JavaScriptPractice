7f20c2bdf1b9ef15515017079e6c7bd07f17a5c1
ArnauDavis <arnau.davis@outlook.com>
2025-03-10 19:49:54 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3542,5 +3542,18 @@ var lengthOfLastWord = function(s) {
   let p1 = s.trim().split(' ')
   return p1[p1.length-1].length
 }
-	
+
+
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ')
+}	
 	
