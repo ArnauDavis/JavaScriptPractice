fa38f65379e4bca3811a5056f7e7caf623422222
ArnauDavis <arnau.davis@outlook.com>
2025-03-13 22:51:24 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3569,4 +3569,11 @@ function divisibleBy(numbers, divisor){
   }	
 
 	
+
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+	//the assignment == was incorrect so I fixed it
 	
