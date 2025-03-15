4fc78d18bff67b652b3d54b2a80a2c245022d8e5
ArnauDavis <arnau.davis@outlook.com>
2025-03-14 20:21:25 -0400

    added a new solved problem
    I used recursion here, pretty cool right?

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3576,4 +3576,32 @@ var a = "code";
 var b = "wa.rs";
 var name = a + b;
 	//the assignment == was incorrect so I fixed it
+
+
+
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+// Examples
+
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digitalRoot(n) {
+  let v = n.toString().split('')
+  let c = v.map((x)=>Number(x))
+  if(c.length > 1){
+    let test = c.reduce(
+  (acc, cur) => acc + cur,
+  0,
+  )
+    c = test
+    return digitalRoot(test)
+  }else{
+    return Number(c.join())
+  }
+}	
+
 	
