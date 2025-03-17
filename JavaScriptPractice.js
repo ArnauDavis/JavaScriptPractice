7296ea6059c0b3824be5c37d44b3bb9a1cf0717c
ArnauDavis <arnau.davis@outlook.com>
2025-03-16 22:09:24 -0600

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3622,6 +3622,37 @@ function descendingOrder(n){
 })
   return Number(ck.join(''))
 }	
+
+
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+// Examples (a, b) --> output (explanation)
+
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+// Your function should only return a number, not the explanation about how you get that number.
+
+function getSum(a, b){
+  let con = []
+  let ans = []
+  con.push(a)
+  con.push(b)
+  con.sort(function(a,b) { 
+    return a - b; 
+})
+  for(let i = con[0]; i <= con[1]; i++){
+    ans.push(i)
+  }
+  return ans.reduce((acc,cur)=>acc+cur,0)
+}	
+	
 	
 
 	
