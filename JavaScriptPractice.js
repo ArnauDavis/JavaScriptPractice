728a34cdc9f2feaaf36eac41acf4a99daa8a8e2a
ArnauDavis <arnau.davis@outlook.com>
2025-03-18 17:16:33 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3675,6 +3675,21 @@ var number = function(busStops){
   }
   return ans
 }	
+
+
+
+// Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+// Examples (input --> output):
+
+// "hello case" --> "HelloCase"
+// "camel case word" --> "CamelCaseWord"
+
+String.prototype.camelCase = function() {
+  let p1 = this.split(' ').filter(Boolean)
+  let p2 = p1.map((x) => x[0].toUpperCase() + x.slice(1).toLowerCase())
+  return p2.join('')
+}	
+	
 	
 	
 
