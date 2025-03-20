3c85f7881d78080d47cf399e588a640606e09eb3
ArnauDavis <arnau.davis@outlook.com>
2025-03-20 17:47:45 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3692,5 +3692,38 @@ String.prototype.camelCase = function() {
 	
 	
 	
+// You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
 
+// Your function will accept three arguments:
+// The first and second argument should be numbers.
+// The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+// if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+// Example:
+
+// arguments: 1, 2, "+"
+// should return 3
+
+// arguments: 1, 2, "&"
+// should return "unknown value"
+
+// arguments: 1, "k", "*"
+// should return "unknown value"
+
+function calculator(a,b,sign){
+  let ch1 = ['+','-','/','*']
+  if(typeof a == 'number' && typeof b == 'number' && ch1.includes(sign) == true ){
+    if(sign == '+'){
+      return a + b
+    }else if (sign == '-'){
+      return a - b
+    }else if (sign == '/'){
+      return a / b
+    }else if (sign == '*'){
+      return a * b
+    }
+  }else{
+    return 'unknown value'
+  }
+}
 	
