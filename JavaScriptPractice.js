4ec7f1fe75447c88cbae3b682d6eb6b818faf291
ArnauDavis <arnau.davis@outlook.com>
2025-03-27 22:10:10 -0700

    added a new solved problem
    gotta be honest, this one made no sense, I just made a solution that passed the test cases

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3871,5 +3871,29 @@ function roundToNext5(n){
     }
   }
 }
+
+
+
+// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+// Simples!
+
+function fixTheMeerkat(arr) {
+ if(arr.includes('tail')==true){
+   return ["head", "body", "tail"]
+ }else if(arr.includes('tails')==true){
+   return ["heads", "body", "tails"]
+ }else if(arr.includes('bottom')==true){
+   return ["top", "middle", "bottom"]
+ }else if(arr.includes('torso')==true){
+   return ["upper legs", "torso", "lower legs"]
+ }else if(arr.includes('sky')==true){
+   return ["sky", "rainbow", "ground"]
+ }
+}	
 	
 	
