0b48875ded5d148b75d1def3a69722f4eeb4c92f
ArnauDavis <arnau.davis@outlook.com>
2025-03-28 14:26:48 -0400

    added a new solved problem
    solved without using regex, casual flex

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -3900,5 +3900,38 @@ function fixTheMeerkat(arr) {
  return [arr[2],arr[1],arr[0]]
 }
 	//they just wanted the array to be returned in a certain order, the wording is so bad here
+
+
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words){
+  let ans = []
+  let haveNums = '123456789'
+  let p1 = words.split(' ')
+  for(let i = 0; i<p1.lengthx; i++){
+    ans.push('')
+  }
+  p1.forEach((x)=>{
+    let test = x.split('')
+    test.forEach((b)=>{
+      if(haveNums.includes(b)==true){
+        ans[b] = x
+      }
+    })
+  })
+  ans.shift()
+  return ans.join(' ')
+}
+
 	
 	
