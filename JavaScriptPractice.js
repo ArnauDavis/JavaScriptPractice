bd1b5c348d3747c7664cff7a2ebd8a312aea582d
ArnauDavis <arnau.davis@outlook.com>
2025-04-12 22:44:06 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+// Example:
+
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+function flattenAndSort(array) {
+  return array.flat().sort((a,b) => a-b)
+}
+
+
+
 // Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
 // Example (Input --> Output)
 
