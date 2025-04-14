947e0242ee37fd571edea5535cae95d759224fdf
ArnauDavis <arnau.davis@outlook.com>
2025-04-13 23:51:23 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,40 @@
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+//     humanYears >= 1
+//     humanYears are whole numbers only
+
+// Cat Years
+
+//     15 cat years for first year
+//     +9 cat years for second year
+//     +4 cat years for each year after that
+
+// Dog Years
+
+//     15 dog years for first year
+//     +9 dog years for second year
+//     +5 dog years for each year after that
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let ans = []
+  if(humanYears>2){
+    ans.push(humanYears,(humanYears-2)*4+9+15,(humanYears-2)*5+9+15)
+  }else if(humanYears>1){
+    ans.push(humanYears,24,24)
+  }else{
+    ans.push(humanYears,15,15)
+  }
+  return ans
+}
+
+
+
 // Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
 
 // Example:
