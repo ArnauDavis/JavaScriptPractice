c3a82253233ce3a6cd9ad204ab9ea2dbb25543d4
ArnauDavis <arnau.davis@outlook.com>
2025-04-16 00:26:00 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. 
+// Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+
+function differenceInAges(ages){
+  let p1 = ages.sort((a,b)=>a-b)
+  return [p1[0],p1[p1.length-1],p1[p1.length-1]-p1[0]]
+}
+
+
+
 // Your task is to write a function which returns the sum of a sequence of integers.
 
 // The sequence is defined by 3 non-negative values: begin, end, step.
