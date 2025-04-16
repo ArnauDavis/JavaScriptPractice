1713e8189686652f9ea3d079907e58c3de0f45bc
ArnauDavis <arnau.davis@outlook.com>
2025-04-15 23:09:05 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,26 @@
+// Your task is to write a function which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+// Examples
+
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+
+const sequenceSum = (begin, end, step) => {
+  let p1 = []
+  for(let i = begin; i<=end; i += step){
+    p1.push(i)
+  }
+  return p1.reduce((acc,cur)=>acc+cur,0)
+}
+
+
+
 // Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
 
 function take(arr, n) {
