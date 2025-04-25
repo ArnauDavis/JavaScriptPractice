0f84f88be9ac49fbc1a009f57f69f2ca3bfd7661
ArnauDavis <arnau.davis@outlook.com>
2025-04-24 23:03:22 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,22 @@
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence 
+// "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string){
+  let bet = ['abcdefghijklmnopqrstuvwxyz']
+  let checking = string.toLowerCase().split('').sort()
+  let bet2 = []
+  checking.forEach((x)=>{
+    if(bet[0].includes(x)&&!bet2.includes(x)){
+      bet2.push(x)
+    }
+  })
+  return bet.join('') == bet2.join('') ? true : false
+}
+
+
+
 // Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
 // If you want to know more: http://en.wikipedia.org/wiki/DNA
