194d5f65c846a18dd4014e32334218c7c5b1c2a6
ArnauDavis <arnau.davis@outlook.com>
2025-04-25 22:45:29 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,20 @@
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+// Examples
+
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+function powersOfTwo(n){
+  let ans = []
+  for(let i=0;i<=n;i++){
+    ans.push(Math.pow(2,i))
+  }
+  return ans
+}
+
+
+
 // A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence 
 // "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 
