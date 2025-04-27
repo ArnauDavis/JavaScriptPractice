b1568ff58758a15179bc136b1c86084d38b2e11c
ArnauDavis <arnau.davis@outlook.com>
2025-04-27 01:02:03 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,12 @@
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. 
+// The strings will not be the same length, but they may be empty ( zero length ).
+
+function solution(a, b){
+  return a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
+}
+
+
+
 // Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 
 // The output should be two capital letters with a dot separating them.
