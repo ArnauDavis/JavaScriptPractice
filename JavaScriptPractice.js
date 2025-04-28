49cb6db5d3fa061e9c44baa177d8c0d060b33853
ArnauDavis <arnau.davis@outlook.com>
2025-04-28 12:25:13 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,24 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Additionally, if the number is negative, return 0.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+  let p1 = []
+  for(let i=0;i<number;i++){
+    if(i%5==0||i%3==0){
+      p1.push(i)
+    }
+  }
+  let p2 = p1.reduce((acc,cur)=>acc+cur,0,)
+  return p2
+}
+
+
+
 // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. 
 // The strings will not be the same length, but they may be empty ( zero length ).
 
