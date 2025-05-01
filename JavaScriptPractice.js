5a45d7dfccd5583e613e25913b0172db6cc27981
ArnauDavis <arnau.davis@outlook.com>
2025-05-01 04:32:41 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,20 @@
+// Implement the function generateRange which takes three arguments (start, stop, step) and returns the range of integers from start to stop (inclusive) in increments of step.
+// Examples
+
+// (1, 10, 1) -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// (-10, 1, 1) -> [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1]
+// (1, 15, 20) -> [1]
+
+function generateRange(min, max, step){
+  let ans = []
+  for(let i = min; i<=max; i+=step){
+    ans.push(i)
+  }
+  return ans
+}
+
+
+
 // Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
 
 // For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
