58e7570fa7f710923674a9cccc01b223855e3e8b
ArnauDavis <arnau.davis@outlook.com>
2025-05-03 02:41:49 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// Given a number n, return the number of positive odd numbers below n
+// Examples (Input -> Output)
+
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+function oddCount(n){
+  return Math.floor(n/2)
+}
+
+
+
 // Write a function that will return the integer number of e-mails sent in the percentage of the limit.
 
 // Example:
