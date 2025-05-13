7b3b9aa9cdd90836fc7f534328b0a3c3459c82f8
ArnauDavis <arnau.davis@outlook.com>
2025-05-12 23:00:35 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,18 @@
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. 
+// Return null/nil/None if input is empty string or null/nil/None.
+
+// Example:
+
+// greet("Niks") === "hello Niks!";
+// greet("") === null; // Return null if input is empty string
+// greet(null) === null; // Return null if input is null
+
+function greet(name) {
+  return name ? `hello ${name}!` : null
+}
+
+
+
 // You will be given a number and you will need to return it as a string in Expanded Form. For example:
 
 //    12 --> "10 + 2"
