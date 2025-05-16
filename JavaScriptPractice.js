c1681a4727587c869d8815330fb193759df8dd3e
ArnauDavis <arnau.davis@outlook.com>
2025-05-15 20:53:30 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,29 @@
+// Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many 
+// 	years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+
+// Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: 
+// 		"You were born this very year!"
+
+// "..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+
+function  calculateAge(bir,cur) {  
+  let age = cur - bir
+  console.log(age)
+  if(age==-1){
+    return 'You will be born in 1 year.'
+  }else if(age<0){
+    return `You will be born in ${age*-1} years.`
+  }else if(age==0){
+    return 'You were born this very year!'
+  }else if(age==1){
+    return 'You are 1 year old.'
+  }else{
+    return `You are ${age} years old.`
+  }
+}
+
+
+
 // Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
 
 // When bool is truthy, func1 should be called, otherwise call the func2.
