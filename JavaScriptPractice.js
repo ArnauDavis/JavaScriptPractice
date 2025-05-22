256360e28a7608e2526ae14ec8b04fef668c1593
ArnauDavis <arnau.davis@outlook.com>
2025-05-21 23:56:49 -1200

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,19 @@
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+// Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum (numbers) {
+  let p1 = numbers.sort((a,b)=> a-b)
+  return p1[p1.length-1]+p1[p1.length-2]
+}
+
+
+
 // Find the mean (average) of a list of numbers in an array.
 
 function findAverage(nums) {
