3c5bacb1561c23ea047fca07cb4a00e53c493415
ArnauDavis <arnau.davis@outlook.com>
2025-05-25 03:51:00 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,13 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds( values ){
+  return values.filter((x)=>x%2==0)
+}
+
+
+
 // Complete the function which returns the weekday according to the input number:
 
 //     1 returns "Sunday"
