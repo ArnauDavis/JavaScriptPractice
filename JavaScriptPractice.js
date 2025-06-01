42dfbd78a017587090514a7360aa89fc73a895f7
ArnauDavis <arnau.davis@outlook.com>
2025-05-31 21:37:22 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,57 @@
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+// For example, decode("h3 th2r2") would return "hi there".
+
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+function encode(string) {
+  const p1 = {
+    'a': 1,
+    'e': 2,
+    'i': 3,
+    'o': 4,
+    'u': 5
+  }
+  const p2 = ['a','e','i','o','u']
+  let p3 = string.split('')
+  p3.forEach((x,i)=>{
+    if(p2.includes(x)==true){
+      p3[i] = p1[x]
+    }
+  })
+  return p3.join('')
+}
+
+function decode(string) {
+  const p1 = {
+    '1': 'a',
+    '2': 'e',
+    '3': 'i',
+    '4': 'o',
+    '5': 'u'
+  }
+  const p2 = ['1','2','3','4','5']
+  let p3 = string.split('')
+  p3.forEach((x,i)=>{
+    if(p2.includes(x)==true){
+      p3[i] = p1[x]
+    }
+  })
+  return p3.join('')
+}
+
+
+
 // Complete the method which accepts an array of integers, and returns one of the following:
 
 //     "yes, ascending" - if the numbers in the array are sorted in an ascending order
