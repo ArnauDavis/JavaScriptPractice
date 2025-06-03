16d4c3e783b50201d573b81c2ff65184438461d9
ArnauDavis <arnau.davis@outlook.com>
2025-06-02 22:21:22 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,60 @@
+// Task
+
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+
+// The other letters don't have power and are only victims. Sum up each side's letters' power values to determine which side wins.
+// Example
+
+// alphabetWar("z");        //=> Right side wins!
+// alphabetWar("zdqmwpbs"); //=> Let's fight again!
+// alphabetWar("zzzzs");    //=> Right side wins!
+// alphabetWar("wwwwwwz");  //=> Left side wins!
+
+function alphabetWar(fight){
+  let c1 = fight.split('')
+  const l1 = ['w','p','b','s']
+  const r1 = ['m','q','d','z']
+  const left = {
+    'w':4,
+    'p':3,
+    'b':2,
+    's':1
+  }
+  const right = {
+    'm':4,
+    'q':3,
+    'd':2,
+    'z':1
+  }
+  let l2 = 0
+  let r2 = 0
+  c1.forEach((x)=>{
+    if(l1.includes(x)){
+      l2 += left[x]
+    }else if(r1.includes(x)){
+      r2 += right[x]
+    }
+  })
+  let a1 = l2 > r2 ? "Left side wins!" : "Right side wins!"
+  return l2 == r2 ? "Let's fight again!" : a1
+}
+
+
+
 // You're on your way to the market when you hear beautiful music coming from a nearby street performer. The notes come together like you wouln't believe as the musician puts together patterns of tunes. As you wonder what kind of 
 // algorithm you could use to shift octaves by 8 pitches or something silly like that, it dawns on you that you have been watching the musician for some 10 odd minutes. You ask, "how much do people normally tip for something like this?" 
 // The artist looks up. "It's always gonna be about tree fiddy."
