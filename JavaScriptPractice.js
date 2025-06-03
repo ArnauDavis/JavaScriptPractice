af433ec4026bf234533cc1314fb0baabb9e654b5
ArnauDavis <arnau.davis@outlook.com>
2025-06-03 16:54:17 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,4 +1,16 @@
-// Task
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+//   3 -->    15  (  3 * 5¹)
+//  10 -->   250  ( 10 * 5²)
+// 200 --> 25000  (200 * 5³)
+//   0 -->     0  (  0 * 5¹)
+//  -3 -->   -15  ( -3 * 5¹)
+
+function multiply(number){
+  return number * (Math.pow(5,number.toString().split('').filter((x)=>x!='-').length))
+}
+
+
 
 // Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
 
