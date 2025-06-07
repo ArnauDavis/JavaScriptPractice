7a7891d3f4af578bfa3868de5002a5cf049276fc
ArnauDavis <arnau.davis@outlook.com>
2025-06-06 23:39:50 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,20 @@
+// You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (always 2 for Java).
+
+// You can not use multiplier "*" operator.
+
+// If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 letters * 30 = 600 (Space counts as a character).
+
+function billboard(name, price = 30){
+  let p1 = name.split('')
+  let cost = 0
+  for(let i=0;i<p1.length;i++){
+    cost+=price
+  }
+  return cost
+} 
+
+
+
 // Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. 
 // Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
 	
