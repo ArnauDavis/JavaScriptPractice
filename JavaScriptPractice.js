4626fcbe40c06cedf5a70f52d739cf33fc4f532d
ArnauDavis <arnau.davis@outlook.com>
2025-06-13 09:18:32 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,35 @@
+// Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+
+// You need to consider the following ratings:
+
+//     Terrible: tip 0%
+//     Poor: tip 5%
+//     Good: tip 10%
+//     Great: tip 15%
+//     Excellent: tip 20%
+
+// The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+
+//     "Rating not recognised" in Javascript, Python and Ruby...
+//     ...or null in Java
+//     ...or -1 in C#
+
+// Because you're a nice person, you always round up the tip, regardless of the service.
+
+function calculateTip(amount, rating) {
+  let p1 = rating.toLowerCase()
+  let percentage = {
+    'terrible': 0,
+    'poor': 0.05,
+    'good': 0.1,
+    'great': 0.15,
+    'excellent': 0.2
+  }
+  return p1 in percentage? Math.ceil(amount * percentage[p1]) : "Rating not recognised"
+}
+
+
+
 // Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
 
 // Example:
