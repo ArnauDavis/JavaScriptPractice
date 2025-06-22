75dd29ca3180646c553dc2e26bb14862246a3d8b
ArnauDavis <arnau.davis@outlook.com>
2025-06-21 22:48:46 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,17 @@
+// For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. 
+// If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+function cookie(x){
+  let check = typeof(x)
+  let a1 = {
+    'string' : "Who ate the last cookie? It was Zach!",
+    'number' : "Who ate the last cookie? It was Monica!",
+  }
+  return a1[check]? a1[check] : "Who ate the last cookie? It was the dog!"
+}
+
+
+
 // We want to generate a function that computes the series starting from 0 and ending until the given number.
 // Example:
 
