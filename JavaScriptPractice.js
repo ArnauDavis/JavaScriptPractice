a683b4ccc57bbefb2854f73c086a5b184cef3606
ArnauDavis <arnau.davis@outlook.com>
2025-06-24 23:30:54 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,25 @@
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+// [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+function multipleOfIndex(array) {
+  let ans = array.filter((x,i)=>{
+    if(x==0){
+      return x===0
+    }else if(x%i==0){
+      return x
+    }
+  })
+  return ans
+}
+
+
+
 // Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
 
 // Assume that the input n will always be a positive integer.
