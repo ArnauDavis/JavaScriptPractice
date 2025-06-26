8cd82e5972028ab624804a904d983b799d7be184
ArnauDavis <arnau.davis@outlook.com>
2025-06-25 23:12:59 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,27 @@
+// Create a method all which takes two params:
+
+//     a sequence
+//     a function (function pointer in C)
+
+// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. 
+// If the sequence is empty, it should return true, since technically nothing failed the test.
+// Example
+
+// all((1, 2, 3, 4, 5), greater_than_9) -> false
+// all((1, 2, 3, 4, 5), less_than_9)    -> True
+
+function all( arr, fun ){
+  let ans = 0
+  arr.forEach((x)=>{
+    if(!fun(x)){
+      ans+=1
+    }
+  })
+  return ans>0? false : true
+}
+
+
+
 // Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 
