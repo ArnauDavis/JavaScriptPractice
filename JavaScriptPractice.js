a563501501374a016776a61034fdb5e923a4e907
ArnauDavis <arnau.davis@outlook.com>
2025-06-28 21:41:27 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,13 @@
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+function toNumberArray(stringarray){
+  return stringarray.map((x)=>Number(x))
+}
+
+
+
 // Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
 // Examples
 
