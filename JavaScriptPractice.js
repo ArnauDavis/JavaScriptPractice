43cb10561a7ec32f0c82a7897ecc81617f1480d8
ArnauDavis <arnau.davis@outlook.com>
2025-07-03 23:41:01 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,24 @@
+// Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+// Notes
+
+//     Array/list will contain positives only .
+//     Array/list will always have even size
+// Input >> Output Example
+
+// minSum({5,4,2,3}) ==> return (22)
+// 5*2 + 3*4 = 22
+
+function minSum(arr) {
+  let p1 = arr.sort((a,b)=>a-b)
+  let ans = 0
+  for(let i=0;i<p1.length/2;i++){
+    ans+=(p1[i]*p1[p1.length-1-i])
+  }
+  return ans
+}
+
+
+
 // An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
 // Example: (Input --> Output)
