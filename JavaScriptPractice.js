69100acfb43edf2ad395e2b5cf61467368204a67
ArnauDavis <arnau.davis@outlook.com>
2025-07-04 23:15:07 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,33 @@
+// Strong number is a number with the sum of the factorial of its digits is equal to the number itself.
+
+// For example, 145 is strong, because 1! + 4! + 5! = 1 + 24 + 120 = 145.
+// Task
+
+// Given a positive number, find if it is strong or not, and return either "STRONG!!!!" or "Not Strong !!".
+// Examples
+
+//     1 is a strong number, because 1! = 1, so return "STRONG!!!!".
+//     123 is not a strong number, because 1! + 2! + 3! = 9 is not equal to 123, so return "Not Strong !!".
+//     145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
+//     150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
+
+function strong(n) {
+  let p1 = n.toString().split('')
+  let p2 = 0
+  p1.forEach((x)=>{
+    let p3 = Number(x)
+    let h1 = 1
+    for(let i=1;i<p3+1;i++){
+      h1*=i
+    }
+    p2+=h1
+    console.log(h1)
+  })
+  return n >= p2? "STRONG!!!!" : "Not Strong !!"
+}
+
+
+
 // Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
 // Notes
 
