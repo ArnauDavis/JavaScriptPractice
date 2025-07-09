1670b87729709383ff9aa40f4a4e3dd4245d7449
ArnauDavis <arnau.davis@outlook.com>
2025-07-09 02:06:59 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,19 @@
+// Your task is to implement the authenticate() method of the sleigh, which takes the name of the person, who wants to board the sleigh and a secret password. 
+// If, and only if, the name equals "Santa Claus" and the password is "Ho Ho Ho!" (yes, even Santa has a secret password with uppercase and lowercase letters and special characters :D),
+// the return value must be true. Otherwise it should return false.
+
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  if(name=="Santa Claus" && password=='Ho Ho Ho!'){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+
 // You have to write a function that describe Leo:
 
 // function leo(oscar) {
