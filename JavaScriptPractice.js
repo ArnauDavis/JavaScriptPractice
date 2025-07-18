844c0f5e48011f9161e5200b6f3f0e925e2dce12
ArnauDavis <arnau.davis@outlook.com>
2025-07-17 23:52:20 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,38 @@
+// Your task is to write a function which returns the n-th term of the following series, which is the sum of the first n terms of the sequence (n is the input parameter).
+// Series:1+14+17+110+113+116+…\mathrm{Series:}\quad 1 + \frac14 + \frac17 + \frac1{10} + \frac1{13} + \frac1{16} + \dotsSeries:1+41​+71​+101​+131​+161​+…
+
+// You will need to figure out the rule of the series to complete this.
+// Rules
+
+//     You need to round the answer to 2 decimal places and return it as String.
+
+//     If the given value is 0 then it should return "0.00".
+
+//     You will only be given Natural Numbers as arguments.
+
+// Examples (Input --> Output)
+
+// n
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  if(n==0){
+    return '0.00'
+  }else{
+    let top = 1
+    let bot = 4
+    for(let i=1;i<n;i++){
+      top += 1/bot
+      bot+=3
+    }
+    return top.toFixed(2)
+  }
+}
+
+
+
 // There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
 
 //     number of pillars (≥ 1);
