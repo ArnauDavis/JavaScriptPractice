36a257d006046d64083ec4532f4f623c3df3f5c2
ArnauDavis <arnau.davis@outlook.com>
2025-07-22 10:52:52 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,32 @@
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. 
+// The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+// Example
+
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+function duplicateCount(text){
+  if(!text){
+    return 0
+  }
+  let p1 = text.toLowerCase().split('').reduce((acc,cur)=>{
+    acc[cur] = (acc[cur] || 0) + 1
+    return acc
+  }, {})
+  let p2 = []
+  Object.keys(p1).forEach(x=>p2.push(p1[x]))
+  let ans = 0
+  p2.forEach((x)=>{if(x>1){ans+=1}})
+  return ans>0? ans : 0
+}
+
+
+
 // Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
 
 // (the dedicated builtin(s) functionalities are deactivated)
