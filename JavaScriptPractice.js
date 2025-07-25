52f57daba19bcae20a3ec5849c446d4f02f66da7
ArnauDavis <arnau.davis@outlook.com>
2025-07-25 10:57:56 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,25 @@
+// Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+// Example
+
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+function contamination(text, char){
+  let ans = []
+  for(let i=0;i<text.length;i++){
+    ans.push(char)
+  }
+  return ans.join('')
+}
+
+
+
 // Given an array/list of integers, find the Nth smallest element in the array.
 // Notes
 
