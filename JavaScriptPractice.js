5ec717ea90d8100901285f87c1b362e8734d30e1
ArnauDavis <arnau.davis@outlook.com>
2025-08-02 13:02:03 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,13 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = []
+
+websites.push('codewars')
+
+
 // You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, 
 // you would like to find out the house number of the people on the other side of the street. The street looks something like this:
 // Street
