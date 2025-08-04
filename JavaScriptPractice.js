2e9bed879fb2cfec032c592e6e36aaa6cdf915a2
ArnauDavis <arnau.davis@outlook.com>
2025-08-03 22:19:14 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,40 @@
+// You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+// Input:
+
+// If, you can read?
+
+// Output:
+
+// India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+// Note:
+
+//     There is a preloaded dictionary that you can use, named NATO. It uses uppercase keys, e.g. NATO['A'] is "Alfa". See comments in the initial code to see how to access it in your language.
+//     The set of used punctuation is ,.!?.
+//     Punctuation should be kept in your return string, but spaces should not.
+//     Xray should not have a dash within.
+//     Every word and punctuation mark should be seperated by a space ' '.
+//     There should be no trailing whitespace
+
+function toNato(words) {
+    // you can access the preloaded NATO dictionary
+    // NATO['A'] === 'Alfa', etc.
+    let ans = []
+    let punc = [',','.','!','?']
+    let p1 = words.toUpperCase().split('')
+    p1.forEach((x)=>{
+      if(NATO[x]){
+        ans.push(NATO[x])
+      }if(punc.includes(x)){
+        ans.push(x)
+      }
+    })
+    return ans.join(' ')
+}
+
+
+
 // Add the value "codewars" to the websites array.
 // After your code executes the websites array should == ["codewars"]
 
