46539a3a8de60fafdae57271f2dd4f501b9d5e28
ArnauDavis <arnau.davis@outlook.com>
2025-08-06 01:19:53 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,13 @@
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+// All inputs will be valid.
+
+function digits(n) {
+  return n.toString().length
+}
+
+
+
 // Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
 // Examples
 
