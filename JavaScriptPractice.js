3c6d7a73f7d01b80add28abac9ed09567b06218e
ArnauDavis <arnau.davis@outlook.com>
2025-08-07 18:55:11 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,27 @@
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+// Notes:
+
+//     Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given. See the examples below for further details.
+
+//     If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+//     Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+// Examples
+
+// // These two objects are preloaded, you need to use them in your code
+// var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+// var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+// aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+// aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+
+function aliasGen(first, last){
+  return firstName[first[0].toUpperCase()] && surname[last[0].toUpperCase()]? `${firstName[first[0].toUpperCase()]} ${surname[last[0].toUpperCase()]}` : "Your name must start with a letter from A - Z."
+}
+
+
+
 // Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
 
 // All inputs will be valid.
