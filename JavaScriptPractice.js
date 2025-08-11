def5c6207c0e8b273eb57fb400941b861d985c0a
ArnauDavis <arnau.davis@outlook.com>
2025-08-10 23:44:20 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,44 @@
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+// Examples:
+
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+function longestConsec(strarr, k) {
+    if(strarr.length == 0||k>strarr.length||k<=0){
+      return ""
+    }
+    let ans = ""
+    for(let i=0;i<=strarr.length-k;i++){
+        let p1 = ""
+        for(let j=0;j<k;j++){
+        p1+=strarr[i+j]
+      }
+      if (p1.length > ans.length) {
+        ans = p1;
+      }
+    }
+  return ans
+}
+
+
+
 // Given a positive number, determine if it is Automorphic or not. If it is, return "Automorphic", otherwise return "Not!!"
 // Examples
 
