7a0054b1e5810a4e95168bf276d8ec708564bf24
ArnauDavis <arnau.davis@outlook.com>
2025-08-12 18:55:40 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,49 @@
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+function correctPolishLetters (string) {
+  let fix = {
+    'ą' : 'a',
+    'ć' : 'c',
+    'ę' : 'e',
+    'ł' : 'l',
+    'ń' : 'n',
+    'ó' : 'o',
+    'ś' : 's',
+    'ź' : 'z',
+    'ż' : 'z'
+  }
+  let check = ['ą','ć','ę','ł','ń','ó','ś','ź','ż']
+  let ans = []
+  string.split('').forEach((x)=>{
+    if(check.includes(x)){
+      ans.push(fix[x])
+    }else{
+      ans.push(x)
+    }
+  })
+  return ans.join('')  
+}
+
+
+
 // Given an array of ones and zeroes, convert the equivalent binary value to an integer.
 
 // Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
