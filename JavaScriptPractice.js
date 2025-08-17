ff08cfe3e0974476706c357dd3d2286e1a2233c1
ArnauDavis <arnau.davis@outlook.com>
2025-08-16 23:54:17 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,12 @@
+// You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps 
+// returning 10. Fix the function to make it return 32 without changing the number or the operators.
+
+function orderOperations () {
+  return (2 + 2) * (2 + 2) * 2
+}
+
+
+
 // John has invited some friends. His list is:
 
 // s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
