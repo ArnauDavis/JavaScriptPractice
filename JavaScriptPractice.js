0a04353de6325cc0e2d4635b60caeb60b547e47e
ArnauDavis <arnau.davis@outlook.com>
2025-08-24 14:28:40 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,23 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2, there are 4 multiplications)
+// 4 --> 0 (because 4 is already a one-digit number, there is no multiplication)
+
+function persistence(num, count=0) {
+   let p1 = num.toString().split('')
+   if(p1.length<=1){
+     return count
+   }
+   let p2 = p1.map((x)=>Number(x))
+   let p3 = p2.reduce((acc,cur)=>acc*cur,1).toString().split('')
+   return persistence(Number(p3.join('')),count+=1)
+}
+
+
+
 // Sort the Gift Code
 
 // Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
