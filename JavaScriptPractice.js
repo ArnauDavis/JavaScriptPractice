aef81a8dfd966e23ae1435d41907cfe1e2fe67b9
ArnauDavis <arnau.davis@outlook.com>
2025-08-29 11:51:33 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,17 @@
+// You will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+
+// Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per 
+// litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+
+function fuelPrice(litres, pricePerLitre) {
+  for(i = 2; i <= 10; i += 2)
+    if(litres >= i)
+      pricePerLitre -= .05
+  return Number((litres * pricePerLitre).toFixed(2))
+}	
+
+
+
 // You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
 // (If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
 
