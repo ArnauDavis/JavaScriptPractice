10722e0f5cb85203de5798dd78ddb59aa56ff34f
ArnauDavis <arnau.davis@outlook.com>
2025-08-31 12:11:09 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// Point objects have attributes x and y.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Input coordinates fit in range −50⩽x,y⩽50 -50 \leqslant x,y \leqslant 50 −50⩽x,y⩽50. Tests compare expected result and actual answer with tolerance of 1e-6.
+
+function distanceBetweenPoints(a, b) {
+  return Math.sqrt((Math.pow(a['x']-b['x'],2))+(Math.pow(a['y']-b['y'],2)),2)
+}
+
+
+
 // Your task is to sum the differences between consecutive pairs in the array in descending order.
 // Example
 
