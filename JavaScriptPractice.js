8a76fffe61ebb96755efcd98a7fec0702a554e49
ArnauDavis <arnau.davis@outlook.com>
2025-09-01 23:39:11 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,22 @@
+// mission 1:
+
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+// mission 2:
+
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+// mission 3:
+
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+
+function helloWorld(){
+  const str = 'Hello World!'
+  console.log(str)
+}
+
+
+
 // Point objects have attributes x and y.
 
 // Write a function calculating distance between Point a and Point b.
