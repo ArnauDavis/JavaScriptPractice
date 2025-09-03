6445b0927a44785da5eba481dc8435d3d8179e5f
ArnauDavis <arnau.davis@outlook.com>
2025-09-02 22:27:51 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,51 @@
+/*
+Given an array of positive integers (the weights of the people), return a new array / tuple of two integers (depending on your language), whereby the first one is the total weight of team 1, and the second one is the total weight of team 2. Note that the array will never be empty.
+Examples
+
+[13, 27, 49] returns [62, 27] or (62, 27) (depending on your language) because the total weight of team 1 is 
+13
++
+49
+=
+62
+13+49=62 and the total weight of team 2 is 
+27
+27.
+[50, 60, 70, 80] returns [120, 140] or (120, 140) (depending on your language) because the total weight of team 1 is 
+50
++
+70
+=
+120
+50+70=120 and the total weight of team 2 is 
+60
++
+80
+=
+140
+60+80=140.
+[80] returns [80, 0] or (80, 0) (depending on your language) because the total weight of team 1 is 
+80
+80 and the total weight of team 2 is 
+0
+0.
+*/
+
+function rowWeights(array){
+  let p1 = 0
+  let p2 = 0
+  array.forEach((x,i)=>{
+    if(i%2==0){
+     p2+=x
+    }else{
+    p1+=x
+    }
+  })
+  return [p2,p1]
+}
+
+
+
 // mission 1:
 
 // use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
