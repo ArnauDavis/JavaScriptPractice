6c28f7e6bdf167c39d45cc71bb194588c85b911f
ArnauDavis <arnau.davis@outlook.com>
2025-09-11 13:15:01 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,11 @@
+// Get ASCII value of a character.
+
+function getASCII(c){
+  return c.charCodeAt(0)
+}
+
+
+
 // Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
 
 // Note: Input will either be a positive integer (or a string for untyped languages).
