2dcc7ebf4d8639506d9574137d6d987c200c2a9b
ArnauDavis <arnau.davis@outlook.com>
2025-09-15 23:48:00 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,32 @@
+// Write a function called calculate that takes 3 values. The first and third values are numbers. The second value is a character. If the character is "+" , "-", "*", or "/", 
+// the function will return the result of the corresponding mathematical function on the two numbers. If the string is not one of the specified characters, the function should return null (throw an ArgumentException in C#).
+
+// calculate(2,"+", 4); //Should return 6
+// calculate(6,"-", 1.5); //Should return 4.5
+// calculate(-4,"*", 8); //Should return -32
+// calculate(49,"/", -7); //Should return -7
+// calculate(8,"m", 2); //Should return null
+// calculate(4,"/",0) //should return null
+
+// Keep in mind, you cannot divide by zero. If an attempt to divide by zero is made, return null (throw an ArgumentException in C#)/(None in Python).
+
+function calculate(num1, operation, num2) {
+  const c1 = ['+','-','*','/']
+ let calc = {
+   '+': num1+num2,
+   '-': num1-num2,
+   '*': num1*num2,
+   '/': num1/num2,
+ }
+  if(calc[operation]!=-Infinity && c1.includes(operation)){
+    return calc[operation]
+  }else{
+    return null
+  }
+}
+
+
+
 // Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
 
 // For example (Input --> Output)
