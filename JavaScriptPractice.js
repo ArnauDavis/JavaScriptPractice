2d11894096c9849fb5f877c416ec244c1f2b754a
ArnauDavis <arnau.davis@outlook.com>
2025-09-18 23:26:47 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,17 @@
+// Given a non-negative integer b, write a function which returns an integer d such that the binary representation of b is the same as the decimal representation of d.
+
+// Examples:
+
+// n = 1 should return 1
+// n = 5 should return 101
+// n = 11 should return 1011
+
+function toBinary(n){
+  return Number(n.toString(2))
+}
+
+
+
 // Define a method/function that removes from a given array of integers all the values contained in a second array.
 // Examples (input -> output):
 
