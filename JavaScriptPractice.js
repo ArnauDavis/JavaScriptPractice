3750c0a8c094159bd4a346808fdc9b79a1896b85
ArnauDavis <arnau.davis@outlook.com>
2025-09-20 21:02:00 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+// Examples:
+
+// 42, "number"   --> true
+// "42", "number" --> false
+
+function typeValidation(variable, type) {
+  return typeof(variable) == type
+}
+
+
+
 // Return the type of the sum of the two arguments
 
 function typeOfSum(a, b) {
