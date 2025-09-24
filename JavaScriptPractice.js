b3119fab84bc479d296449d2b04f6e48c2fc895e
ArnauDavis <arnau.davis@outlook.com>
2025-09-23 22:44:54 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,23 @@
+// Given an array of 4 integers
+// [a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+
+// For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+
+//    a:x1
+//    b:y1
+//    c:x2
+//    d:y2
+
+// Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). 
+
+function slope(points){
+  let p=points
+  let ans=((p[3]-p[1])/(p[2]-p[0])).toString()
+  return Number(Math.abs(ans))<Infinity? ans : 'undefined'
+  }
+
+
+
 // You are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
 
 // Equation will be the form of ax^2 + bx + c = 0
