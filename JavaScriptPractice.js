10f4e6ed1747d13da7ccd4f0f1f5769d2c78bfb4
ArnauDavis <arnau.davis@outlook.com>
2025-09-24 11:59:19 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,32 @@
+// Complete function padIt, which accepts 2 parameters:
+
+//     str: a string representing the string to pad. We will pad it with "*" symbols on its left side and on its right side.
+//     n: a number indicating how many times to pad the string.
+
+// Behaviour
+
+// You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, 
+// the second time will add a * to the right side, and so on.
+
+// Finally, return the padded string.
+
+function padIt(str,n){
+  let i = 1
+  let ans = [str]
+  while(i<=n){
+    if(i%2!=0){
+      ans.unshift('*')
+      i+=1
+    }else{
+      ans.push('*')
+      i+=1
+    }
+  }
+  return ans.join('')
+}
+
+
+
 // Given an array of 4 integers
 // [a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
 
