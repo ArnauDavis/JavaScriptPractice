6fbb686711490f78b9d0f870f096f230b0fd87d3
ArnauDavis <arnau.davis@outlook.com>
2025-09-27 14:19:41 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,21 @@
+// You are creating an "Escape the room" game. The first step is to create a hash table ( dict in Python) called rooms that contains all of the rooms of the game. 
+// There should be at least 3 rooms inside it, each room being a hash table with at least 3 properties (e.g. name, description, completed).
+
+class Room  {
+  constructor() {
+    this.name = '';
+    this.description = '';
+    this.completed= '';
+  }
+}
+var rooms = {
+  bedroom : new Room(),
+  kitchen : new Room(),
+  bathroom : new Room(),
+}
+
+
+
 // Function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. 
 // Note that an empty string, or no arguments, should return an empty string.
 
