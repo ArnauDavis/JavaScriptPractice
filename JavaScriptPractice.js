a4eed649d18b7afc5aae26db85eb377d03c2046e
ArnauDavis <arnau.davis@outlook.com>
2025-09-29 21:57:54 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,28 @@
+// Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. 
+// For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits 
+// should returns an array of strings where all the rotten fruits are replaced by good ones.
+// Notes
+
+//     If the array is null/nil/None or empty you should return empty array ([]).
+//     The rotten fruit name will be in this camelcase (rottenFruit).
+//     The returned array should be in lowercase.
+
+function removeRotten(bagOfFruits){
+  if(!bagOfFruits){
+    return []
+  }
+  let ans = bagOfFruits.map((x)=>{
+    if(x.length>=6 && x.slice(0,6)=='rotten'){
+      return x.slice(6,x.length).toLowerCase()
+    }else{
+      return x
+    }
+  })
+  return ans
+}
+
+
+
 // It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, 
 // and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
 
