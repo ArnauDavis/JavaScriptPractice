acd90a1293e2175552a47c5346bcdf2e77de705d
ArnauDavis <arnau.davis@outlook.com>
2025-10-15 23:16:30 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,81 @@
+// Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+
+// More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+
+// Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+// For example:
+
+// toLeetSpeak("LEET") returns "1337"
+
+// In this kata we use a simple LeetSpeak dialect. Use this alphabet:
+
+// {
+//   A : '@',
+//   B : '8',
+//   C : '(',
+//   D : 'D',
+//   E : '3',
+//   F : 'F',
+//   G : '6',
+//   H : '#',
+//   I : '!',
+//   J : 'J',
+//   K : 'K',
+//   L : '1',
+//   M : 'M',
+//   N : 'N',
+//   O : '0',
+//   P : 'P',
+//   Q : 'Q',
+//   R : 'R',
+//   S : '$',
+//   T : '7',
+//   U : 'U',
+//   V : 'V',
+//   W : 'W',
+//   X : 'X',
+//   Y : 'Y',
+//   Z : '2'
+// }
+
+function toLeetSpeak(str) {
+  let p1 = str.split('')
+  let db = {
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+  T : '7',
+  U : 'U',
+  V : 'V',
+  W : 'W',
+  X : 'X',
+  Y : 'Y',
+  Z : '2',
+  ' ': ' '
+}
+  let p2 = p1.map((x)=> x.replace(x,db[x.toUpperCase()]))
+  return p2.join('')
+}
+
+
+
 // Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
 
 // Example:
