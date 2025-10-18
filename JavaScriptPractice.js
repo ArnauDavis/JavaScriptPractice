a682cffbf51b41042eddd004e7081937d6f36d0e
ArnauDavis <arnau.davis@outlook.com>
2025-10-18 17:16:05 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,36 @@
+// Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+// Notes
+
+//     Array/list size is at least 2 .
+
+//     Array/list's numbers Will be only Positives
+
+//     Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+
+// productArray ({1,5,2}) ==> return {10,2,5}
+
+// Explanation:
+
+//     The first element 10 is the product of all array's elements except the first element 1
+
+//     The second element 2 is the product of all array's elements except the second element 5
+
+//     The Third element 5 is the product of all array's elements except the Third element 2.
+
+function productArray(numbers) {
+//multiply each number except the current iteration of the loop
+  let ans = []
+  for(let i=0;i<numbers.length;i++){
+  let p1 = numbers.filter((e, index) => index != i)
+  ans.push(p1.reduce((acc,cur)=>acc*cur,1))
+ }
+  return ans
+}
+
+
+
 // A stream of data is received and needs to be reversed.
 
 // Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
