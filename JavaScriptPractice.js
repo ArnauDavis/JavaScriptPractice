f03610a29f936c326ab4d0482a3825f9e20caabf
ArnauDavis <arnau.davis@outlook.com>
2025-10-23 00:59:26 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,26 @@
+// Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+
+// Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+// Notes:
+
+//     return an empty array if your array is empty
+//     arrays will only contain numbers so don't worry about checking that
+
+// Examples:
+
+// [1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
+
+// [4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
+//                                        #  9+3 = 12  -->  2
+
+function incrementer(nums) { 
+  let ans = []
+  nums.forEach((x,i)=>ans.push(Number((x+1+i).toString().slice(-1))))
+  return ans
+}
+
+
+
 // You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character.
 // It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
 
