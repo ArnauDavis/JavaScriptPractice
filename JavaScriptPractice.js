adcf333636782d7f472326bee117fb2947e905df
ArnauDavis <arnau.davis@outlook.com>
2025-11-06 21:54:39 -0500

    added a new way of solving a problem
    optimized a solution

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -20,6 +20,18 @@ function jumpingNumber(n){
   return 'Jumping!!'
 }
 
+//or 
+
+function jumpingNumber(n){
+  let arr = n.toString().split('')
+  for(i=0; i < arr.length-1; i++){
+    if(Math.abs(arr[i] - arr[i+1]) !== 1 ){
+      return 'Not!!'
+    }
+  }
+  return 'Jumping!!'
+}
+
 
 
 // You are given a string of letters and an array of numbers.
