7db8904d8c9015b1a957c943845f6d7ab2e94471
ArnauDavis <arnau.davis@outlook.com>
2025-11-17 23:48:07 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,44 @@
+// Input:
+
+//     a string strng
+//     an array of strings arr
+
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+//     a boolean true if all rotations of strng are included in arr
+//     false otherwise
+
+// Examples:
+
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+
+// Note:
+
+// Though not correct in a mathematical sense
+
+//     we will consider that there are no rotations of strng == ""
+//     and for any array arr: contain_all_rots("", arr) --> true
+
+function containAllRots(strng, arr) {
+    let dec = 0
+    let con = []
+    for(let i=0;i<strng.length;i++){
+      con.push(strng.substr(i) + strng.substr(0, i))
+    }
+  con.forEach((x)=>{
+    if(arr.includes(x)){
+      dec+=1
+    }
+  })
+  return dec == strng.length? true : false
+}
+
+
+
 // We need to write some code to return the original price of a product, the return type must be of type decimal and the number must be rounded to two decimal places.
 
 // We will be given the sale price (discounted price), and the sale percentage, our job is to figure out the original price.
