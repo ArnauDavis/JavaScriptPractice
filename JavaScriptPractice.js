0a651b603fe8648078585071a199c98a5650d107
ArnauDavis <arnau.davis@outlook.com>
2025-11-19 21:59:34 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,30 @@
+// As you plunge out of the slide into the water, you see a shark hovering in the darkness under the pontoon... Crap!
+
+// You need to work out if the shark will get to you before you can get to the pontoon. To make it easier... as you do the mental calculations in the water you either freeze when you realise you are dead, or swim when you realise you can make it!
+
+// You are given 5 variables;
+
+//     sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+
+//     sharkSpeed = how fast it can move in metres/second.
+
+//     pontoonDistance = how far you need to swim to safety in metres.
+
+//     youSpeed = how fast you can swim in metres/second.
+
+//     dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+//     The pontoon, you, and the shark are all aligned in one dimension.
+
+// If you make it, return "Alive!", if not, return "Shark Bait!".
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+  let p1 = dolphin? sharkSpeed/2 : sharkSpeed
+  return pontoonDistance/youSpeed > sharkDistance/p1? "Shark Bait!" : "Alive!"
+}
+
+
+
 // Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
 
 // snoopy.bark(); // return "Woof"
