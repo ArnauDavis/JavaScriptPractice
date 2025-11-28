2ccedff74e0d81d49a3a25e4ea1c0137d4a07895
ArnauDavis <arnau.davis@outlook.com>
2025-11-28 18:26:32 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,31 @@
+// Given a string, return a new string that has transformed based on the input:
+
+//     Change case of every character, ie. lower case to upper case, upper case to lower case.
+//     Reverse the order of words from the input.
+
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+
+// You may assume the input only contain English alphabet and spaces.
+
+function stringTransformer(str) {
+  let p1 = str.split(' ').reverse().map((x)=>{
+      return x.split('').map((x)=>{
+        if(x==x.toUpperCase()){
+          return x.toLowerCase()
+        }else{
+          return x.toUpperCase()
+        }
+      }).join('')
+  }).join(' ')
+  return p1 
+}
+
+
+
 // Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
 // Notes
 
