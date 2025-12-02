79e52145caa2b556e2a46b361d8dfee5cd00e16c
ArnauDavis <arnau.davis@outlook.com>
2025-12-01 23:43:54 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,18 @@
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+// Examples
+
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+function calculate(str) {
+  let p1 = str.replaceAll('plus','+').replaceAll('minus','-')
+  return eval(p1).toString()
+}
+
+
+
 // Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
 // Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
