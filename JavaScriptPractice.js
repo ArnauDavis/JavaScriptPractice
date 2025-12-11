1bcdd9185d5591fd390549c0e7b9dab23984a9e9
ArnauDavis <arnau.davis@outlook.com>
2025-12-11 00:52:15 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,21 @@
+// Coding in function colorOf. function accept 3 parameter:r g b. It means value of color red green and blue. the value range is 0-255.
+
+// Use toString(16) Convert numbers r g b to hex string form. at last, combine them to a web color code and return it.
+
+// the color code should starting with "#". and then use 2 characters per color.
+
+// for example:
+
+// colorOf(255,0,0) should return "#ff0000"
+// colorOf(0,111,0) should return "#006f00"
+// colorOf(1, 2 ,3) should return "#010203"
+
+function colorOf(r,g,b){
+  return `#${(r).toString(16).padStart(2, "0")}${(g).toString(16).padStart(2, "0")}${(b).toString(16).padStart(2, "0")}`
+}
+
+
+
 // Let's imagine we have a popular online RPG. A player begins with a score of 0 in class E5. A1 is the highest level a player can achieve.
 
 // Now let's say the players wants to rank up to class E4. To do so the player needs to achieve at least 100 points to enter the qualifying stage.
