8b5bb74a22adb7d0c0349ec555d31ef9843f1f7d
ArnauDavis <arnau.davis@outlook.com>
2025-12-17 22:21:02 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,37 @@
+// Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+// Example ticket:
+
+// [ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+
+// To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, 
+// you get a mini win. Note you can only have one mini win per sub array.
+
+// Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+// All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+function bingo(ticket, win){
+  if(!ticket){
+    return 'Loser!'
+  }
+  //for each x -> [ 'ABC', 65 ]
+  //x[0].split('').forEach((x)=>{if(x.charCodeAt(0)==x[1]){return miniWin+=1})})
+  let score = 0
+  ticket.forEach((x)=>{
+    let check = 0
+    x[0].split('').forEach((y)=>{
+      if(y.charCodeAt(0) == x[1] && check<=0){
+        check+=1
+        score+=1
+      }
+    })
+  })
+  return score>=win? "Winner!" : "Loser!"
+}
+
+
+
 // If a number is even, convert it to binary.
 // If a number is odd, convert it to hex.
 
