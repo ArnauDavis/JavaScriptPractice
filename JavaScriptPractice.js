6d8077f2aaa8fdf5aa4c7816bfcc4343be1b53e2
ArnauDavis <arnau.davis@outlook.com>
2025-12-19 23:20:27 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,12 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin) {
+  let p1 = Number(bin)
+  return parseInt(bin, 2)
+}
+
+
+
 // Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
 
 // flatten([1,2,3])  ==> [1,2,3]
