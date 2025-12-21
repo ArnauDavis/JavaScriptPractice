1dc15f92f80ccacb3a2a496a3db1dbb46ccc3c87
ArnauDavis <arnau.davis@outlook.com>
2025-12-21 13:38:42 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,13 @@
+// You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+// It is guaranteed that a and b are both present in arr.
+
+function consecutive(arr, a, b) {
+  return Math.abs(arr.indexOf(b)-(arr.indexOf(a)))==1
+}
+
+
+
 // It is easy to join two strings together like this string1 + string2.
 
 // Another way to get the desired result would be with string1.concat(string2)
