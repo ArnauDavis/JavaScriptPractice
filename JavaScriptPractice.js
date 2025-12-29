d86ed9d1337367ddd0fdf056dbf865ad7fc2f8f1
ArnauDavis <arnau.davis@outlook.com>
2025-12-28 23:39:55 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,19 @@
+// Write a function to find if a number is lucky or not. If the sum of all digits is 0 or multiple of 9 then the number is lucky.
+
+// 1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisible by 9, hence number is lucky.
+
+// Function will return true for lucky numbers and false for others.
+
+function isLucky(n) {
+  let p1 = n.toString().split('').map((x)=>Number(x)).reduce((acc,cur)=>acc+cur,0)
+  if(p1==0){
+     return true
+     }
+  return p1%9==0
+}
+
+
+
 // Write a function that returns the number of arguments it received.
 
 // args_count() --> 0
