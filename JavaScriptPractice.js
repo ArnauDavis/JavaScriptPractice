6500e2543dda277bb16a3590a6ef0d9d23b72617
ArnauDavis <arnau.davis@outlook.com>
2025-12-30 13:23:18 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,23 @@
+// Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+
+// For example,
+
+// ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+// [7,70,17]
+
+var filterLucky=x=>{
+  let ans = []
+  x.forEach((x)=>{
+    let p1 = x.toString()
+    if(p1.includes('7')){
+      ans.push(x)
+    }
+  })
+  return ans
+}
+
+
+
 // Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
 
 // The array will never be empty.
