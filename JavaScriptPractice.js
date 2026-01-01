3609101ae2fce6978b7eeb4f612f62a5bffff056
ArnauDavis <arnau.davis@outlook.com>
2026-01-01 13:10:39 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,24 @@
+// A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. 
+// If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+
+// Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+
+// Example:
+
+// "123"      -> true
+// "8409"     -> true
+// "100853"   -> false
+// "33333333" -> true
+// "7"        -> false
+// Try to avoid using the % (modulo) operator. 
+
+function divisibleByThree(str){
+  let p1 = (str.split('').map((x)=>Number(x)).reduce((acc,cur)=>acc+cur,0)/3).toString().split('')
+  return !p1.includes('.')
+}
+
+
+
 // Given a string of space separated words, return the longest word.
 // If there are multiple longest words, return the rightmost longest word.
 // Examples
