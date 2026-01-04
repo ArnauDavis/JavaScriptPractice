2e51ffa08cd61227419a8f385daec3649ccfcf25
ArnauDavis <arnau.davis@outlook.com>
2026-01-03 23:28:56 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,18 @@
+// Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, 
+// complete the get Info property and getInfo method/Info getter which should return johns age is 34
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  get info(){
+    return `${this.name}s age is ${this.age}`
+  }
+}
+
+
+
 // A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. 
 // If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
 
