add469b849d1e9c3ce8ff63745ff5a5698b1f2d1
ArnauDavis <arnau.davis@outlook.com>
2026-01-05 22:20:48 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,28 @@
+// Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+// The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+// Example:
+
+// solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+function solution(pairs){
+  let keys = Object.keys(pairs)
+  let values = Object.values(pairs)
+  let ans = ""
+  for(let i=0;i<keys.length;i++){
+    ans+=`${keys[i]} = ${values[i]},`
+  }
+  return ans.slice(0, -1)
+}
+//or
+const solution = pairs =>
+    Object.entries(pairs)
+        .map(([key, value]) => `${key} = ${value}`)
+        .join(',')
+
+
+
 // Bob is a lazy man.
 
 // He needs you to create a method that can determine how many letters (both uppercase and lowercase ASCII letters) and digits are in a given string.
