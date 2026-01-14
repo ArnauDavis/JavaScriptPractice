16a8c12686fa028a438d419f70d9d7acd8040fd8
ArnauDavis <arnau.davis@outlook.com>
2026-01-14 01:09:26 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,11 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+sortme = function( names ){
+  return names.sort()
+}
+
+
+
 // Coding in function grabDoll. function accept 1 parameter:dolls. it's a string array, a list of some dolls.
 
 // You need traverse dolls by using for loop. If element is "Hello Kitty" or "Barbie doll", you should push it to a bag(bag is an array, I've defined in the function); if it's other strings, we should use continue skip it.
