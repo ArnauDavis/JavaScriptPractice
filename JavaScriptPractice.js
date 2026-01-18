1f790621a67d731fda578ca89dc2b9626fefefb0
ArnauDavis <arnau.davis@outlook.com>
2026-01-17 23:59:08 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,33 @@
+// Write a function that will check if two given characters are the same case.
+
+//     If either of the characters is not a letter, return -1
+//     If both characters are the same case, return 1
+//     If both characters are letters, but not the same case, return 0
+
+// Examples
+
+// 'a' and 'g' returns 1
+
+// 'A' and 'C' returns 1
+
+// 'b' and 'G' returns 0
+
+// 'B' and 'g' returns 0
+
+// '0' and '?' returns -1
+
+function sameCase(a, b){
+  if (a.toUpperCase() === a.toLowerCase() || b.toUpperCase() === b.toLowerCase()) {
+    return -1
+  }else if((a === a.toUpperCase() && b === b.toUpperCase()) || (a === a.toLowerCase() && b === b.toLowerCase())){
+    return 1
+  }else{
+    return 0
+  }
+}
+
+
+
 // For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
 
 // Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell 
