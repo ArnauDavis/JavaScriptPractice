211bb3e69fbc3c636f50dee3ad9076917349e5ad
ArnauDavis <arnau.davis@outlook.com>
2026-01-22 02:20:17 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,22 @@
+// Write a function that returns the number of occurrences of an element in an array.
+
+// This function will be defined as a property of Array with the help of the method Object.defineProperty, which allows to define a new method directly on the object (more info about that you can find on MDN).
+// Examples
+
+// var arr = [0, 1, 2, 2, 3];
+// arr.numberOfOccurrences(0) === 1;
+// arr.numberOfOccurrences(4) === 0;
+// arr.numberOfOccurrences(2) === 2;
+// arr.numberOfOccurrences(3) === 1;
+
+Object.defineProperty(Array.prototype, 'numberOfOccurrences',{ 
+  value : function numberOfOccurrences(element) {
+    return this.filter((x)=>x==element).length
+  }
+})
+
+
+
 // HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
 
 // The sorting should NOT be case sensitive
