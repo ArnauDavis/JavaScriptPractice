3db035fd79372e29742ff8cbdf18dfea52ac8d86
ArnauDavis <arnau.davis@outlook.com>
2026-01-29 02:22:49 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,50 @@
+// You and a group of friends are earning some extra money in the school holidays by re-painting the numbers on people's letterboxes for a small fee.
+
+// Since there are 10 of you in the group each person just concentrates on painting one digit! For example, somebody will paint only the 1's, somebody else will paint only the 2's and so on...
+
+// But at the end of the day you realise not everybody did the same amount of work.
+
+// To avoid any fights you need to distribute the money fairly. That's where this Kata comes in.
+// Kata Task
+
+// Given the start and end letterbox numbers, write a method to return the frequency of all 10 digits painted.
+// Example
+
+// For start = 125, and end = 132
+
+// The letterboxes are
+
+//     125 = 1, 2, 5
+//     126 = 1, 2, 6
+//     127 = 1, 2, 7
+//     128 = 1, 2, 8
+//     129 = 1, 2, 9
+//     130 = 1, 3, 0
+//     131 = 1, 3, 1
+//     132 = 1, 3, 2
+
+// The digit frequencies are:
+
+//     0 is painted 1 time
+//     1 is painted 9 times
+//     2 is painted 6 times
+//     etc...
+
+// and so the method would return [1,9,6,3,0,1,1,1,1,1]
+// Notes
+
+//     0 < start <= end
+
+var paintLetterboxes = function(start, end) {
+  let ans = [0,0,0,0,0,0,0,0,0,0]
+  for(let i=start;i<=end;i++){
+    let p1=i.toString().split('').forEach((x)=>ans[Number(x)]+=1)
+  }
+  return ans
+}
+
+
+
 // Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ... z = 26.
 
 // You will be given a list of strings and your task will be to return the values of the strings as explained above multiplied by the position of that string in the list. For our purpose, position begins with 1.
