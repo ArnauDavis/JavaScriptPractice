e40a299639fbbf00d770bc1c880c45f61e8e390f
ArnauDavis <arnau.davis@outlook.com>
2026-01-30 15:28:50 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,14 @@
+// Write a function that takes two arguments: an array and a callback function (in Ruby: a block).
+
+// The function should return true if the callback / block returns false for all of the items in the array, or if the array is empty; 
+// otherwise return false.
+
+function none(arr, fun){
+  return arr.every(x => !fun(x))
+}
+
+
+
 // You and a group of friends are earning some extra money in the school holidays by re-painting the numbers on people's letterboxes for a small fee.
 
 // Since there are 10 of you in the group each person just concentrates on painting one digit! For example, somebody will paint only the 1's, somebody else will paint only the 2's and so on...
