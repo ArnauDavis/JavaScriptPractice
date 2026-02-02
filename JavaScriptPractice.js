8b7e92eccb026a5825b145bf83c33f7774bb0790
ArnauDavis <arnau.davis@outlook.com>
2026-02-01 22:50:11 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+function twoSort(s) {
+ return s.sort()[0].split('').join('***')
+}
+
+
+
 // You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
 
 // You need to verify that you have some good ones left, in order to prepare for battle:
