424846f4748f7763f178d4747f6e5da46fb044f1
ArnauDavis <arnau.davis@outlook.com>
2026-02-03 23:45:28 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,22 @@
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+// Examples
+
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+function squares(x, n) {
+  if(n<=0){
+    return []
+  }
+  let ans = [x]
+  for(let i=1;i<n;i++){
+    ans.push(Math.pow(ans[ans.length-1],2))
+  }
+  return ans
+}
+
+
+
 // You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
 
 // Given the following input array:
