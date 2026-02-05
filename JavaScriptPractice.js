1acb30d8a91d056ba74ee7d924428ba1ac34bb69
ArnauDavis <arnau.davis@outlook.com>
2026-02-04 20:53:46 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,24 @@
+// You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+
+// The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =. The drone always flies left to right, 
+// and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+
+// Return the resulting lamps string. See example tests for more clarity.
+
+function flyBy(lamps, drone){
+  if(drone.length>=lamps.length){
+    return 'o'.padEnd(lamps.length,'o')
+  }
+  let ans = []
+  for(let i=0;i<drone.length;i++){
+    ans.push('o')
+  }
+  ans.push(lamps.slice(0,lamps.length-drone.length))
+  return ans.join('')
+}
+
+
+
 // Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
 // Examples
 
