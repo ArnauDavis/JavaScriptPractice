d62c6b4cd0de3c0d02f21ec4009d2b4708d1f327
ArnauDavis <arnau.davis@outlook.com>
2026-02-07 00:38:15 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// Write a function that reverses the bits in an integer.
+
+// For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+
+// You can assume that the number is not negative.
+
+function reverseBits(n) {
+  return parseInt(n.toString(2).split('').reverse().join(''),2)
+}
+
+
+
 // Given an array (a list in Python) of integers and an integer n, find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
 
 // If n is not in the given array, return an empty array [].
