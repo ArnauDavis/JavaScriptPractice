a2a89dd84d0a3a2e31aa2f9ac65d68f825a90fab
ArnauDavis <arnau.davis@outlook.com>
2026-02-08 23:56:49 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,14 @@
+// I've hit a few bugs in my Java/Type/Coffee-script code recently, and I've traced the problem back to the global isNaN function I was using. 
+// I had expected it to be more discerning, but it's returning true for undefined right now.
+
+// Write a function isReallyNaN that returns true only if passed in an argument that evalutes to NaN, and returns false otherwise.
+
+const isReallyNaN = (val) => {
+  return val !== val
+}
+
+
+
 // Write a function that reverses the bits in an integer.
 
 // For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
