b1aefda653d823e4f3dd5af4caf4fc194878960c
ArnauDavis <arnau.davis@outlook.com>
2026-02-10 23:39:43 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,23 @@
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). 
+// you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+// for example:
+
+// cutIt(["ab","cde","fgh"]) should return ["ab","cd","fg"]
+// cutIt(["abc","defgh","ijklmn"]) should return ["abc","def","ijk"]
+// cutIt(["codewars","javascript","java"]) should return ["code","java","java"]
+
+function cutIt(arr){
+  let p1 = arr.map((x)=>x.length).sort((a,b)=>a-b)[0]
+  return arr.map((x)=>x.slice(0,p1))
+}
+
+
+
 // Return a string suggesting how many glasses of water you should drink to not be hungover.
 // Examples
 
