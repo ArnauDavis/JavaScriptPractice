5eba1b42515e2ec9512a26c0f6142fb0b32b7a70
ArnauDavis <arnau.davis@outlook.com>
2026-02-15 02:54:50 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,35 @@
+// Count how often sign changes in array.
+// result
+
+// number from 0 to ... . Empty array returns 0
+// example
+
+// const arr = [1, -3, -4, 0, 5];
+
+// /*
+// | elem | count |
+// |------|-------|
+// |  1   |  0    |
+// | -3   |  1    |
+// | -4   |  1    |
+// |  0   |  2    |
+// |  5   |  2    |
+// */
+
+// catchSignChange(arr) == 2;
+
+function catchSignChange(arr) {
+  let ans = 0
+  for(let i=1;i<arr.length;i++){
+    if((arr[i-1]>=0&&arr[i]<0)||(arr[i-1]<0&&arr[i]>=0)){
+      ans+=1
+    }
+  }
+  return ans
+}
+
+
+
 // Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. 
 // The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can 
 // assume both words will contain the dividing letter.
