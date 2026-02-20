87bb7d685dfc56b9450db4b94e00b20bbeece285
ArnauDavis <arnau.davis@outlook.com>
2026-02-20 13:05:12 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,16 @@
+// Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+// Example:
+
+// * With input "The quick brown fox jumps over the lazy dog", 4
+// * Return ['quick', 'brown', 'jumps']
+
+function filterLongWords(sentence, n) {
+  return sentence.split(' ').filter((x)=>x.length>n)
+}
+
+
+
 // Some people just have a first name; some people have first and last names and some people have first, middle and last names.
 
 // You task is to initialize the middle names (if there is any).
