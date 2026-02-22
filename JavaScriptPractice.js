02c3a8bf72a157461e1d0d586271ae41a7701c48
ArnauDavis <arnau.davis@outlook.com>
2026-02-21 23:27:41 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,17 @@
+// Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+
+// Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
+
+// Note that numbers written as strings are strings and must be sorted with the other strings.
+
+function dbSort(a){
+  let p1 = a.filter((x)=>typeof(x)=="number").sort((a,b)=>a-b)
+  let p2 = a.filter((x)=>typeof(x)=="string").sort()
+  return p1.concat(p2)
+}
+
+
+
 // Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
 
 // Example:
