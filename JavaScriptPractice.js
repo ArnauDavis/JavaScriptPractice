ce1926f9bce429776d67d36453b0695d74ad5694
ArnauDavis <arnau.davis@outlook.com>
2026-02-22 10:21:40 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,21 @@
+// My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+
+// "dolphin" -> "The Dolphin"
+
+// However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, 
+// combined into one word (WITHOUT "The" in front), like this:
+
+// "alaska" -> "Alaskalaska"
+
+// Complete the function that takes a noun as a string, and returns her preferred band name written as a string.
+
+function bandNameGenerator(str) {
+  let coinFlip = str[0].toLowerCase()==str[str.length-1].toLowerCase()
+  return coinFlip? str.charAt(0).toUpperCase() + str.slice(1)+str.slice(1) : `The ${str.charAt(0).toUpperCase() + str.slice(1)}`
+}
+
+
+
 // Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
 
 // Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
