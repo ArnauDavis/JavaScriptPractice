de33b8495662650e38ab4b46020ed35d7b03c5dc
ArnauDavis <arnau.davis@outlook.com>
2026-03-01 15:55:06 -0500

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,42 @@
+// The function should take one parameter: an object/dict with two or more name-value pairs that represent the members of the group and the amount spent by each.
+
+// Your function should return an object/dict with the same names, showing how much money the members should pay or receive.
+
+// Further points:
+
+//     The values should be positive numbers if the person should receive money from the group, negative numbers if they owe money to the group.
+//     If value is a decimal, round to two decimal places.
+
+// Translations and comments (and upvotes!) welcome.
+// Example
+
+// 3 friends go out together: A spends £20, B spends £15, and C spends £10. The function should return an object/dict showing that A should receive £5, B should receive £0, and C should pay £5.
+
+// var group = {
+//     A: 20, 
+//     B: 15, 
+//     C: 10
+// }
+
+// splitTheBill(group) // returns {A: 5, B: 0, C: -5}
+
+function splitTheBill(x) {
+    let people = 0
+    let total = 0
+    
+    for (let key in x) {
+      total+= x[key]
+      people+=1
+      }
+    let avg = total/people
+    Object.keys(x).forEach(key => {
+      x[key] = Math.round((x[key] - avg) * 100) / 100
+      })
+    return x
+}
+
+
+
 // Given an integer n and two other values, build an array of size n filled with these two values alternating.
 // Examples
 
