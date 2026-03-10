c76e839efcafdf2a72dab96b6bc13036d5279659
ArnauDavis <arnau.davis@outlook.com>
2026-03-09 23:52:51 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,17 @@
+// Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+// For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+
+function spacey(array) {
+  if (array.length === 0) return []
+  let ans = [array[0]]
+  for(let i=1;i<array.length;i++){
+      ans.push(ans[i-1]+array[i])
+  }
+  return ans
+}
+
+
+
 // Write function potatoes with
 
 //     int parameter p0 - initial percent of water-
