ef9e751be0b20b082fbc81374869c1b8518a2d43
ArnauDavis <arnau.davis@outlook.com>
2026-04-30 23:55:42 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,30 @@
+// Write a function that takes an array of unique integers and returns the minimum number of integers needed to make the values of the array consecutive from the lowest number to the highest number.
+// Example
+
+// [4, 8, 6] --> 2
+// Because 5 and 7 need to be added to have [4, 5, 6, 7, 8]
+
+// [-1, -5] --> 3
+// Because -2, -3, -4 need to be added to have [-5, -4, -3, -2, -1]
+
+// [1] --> 0
+// []  --> 0
+
+function consecutive(array) {
+  if(!array||array.length<2){
+    return 0
+  }
+  let p1 = array.sort((a,b)=>a-b)
+  let p2 = []
+  for(let i=p1[0];i<p1[p1.length-1];i++){
+    p2.push(i)
+  }
+  let p3=p2.length-p1.length+1
+  return p3
+}
+
+
+
 // Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
 
 String.prototype.vowel = function() {
