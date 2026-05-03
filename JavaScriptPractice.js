675629f97830b6184406ffa32defc81dd8187b61
ArnauDavis <arnau.davis@outlook.com>
2026-05-02 23:57:06 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,23 @@
+// Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times 
+// in the list, you can only count it once in your sum.
+
+// For example:
+
+// [ 1, 2, 3] ==> 6
+
+// [ 1, 3, 8, 1, 8] ==> 12
+
+// [ -1, -1, 5, 2, -7] ==> -1
+
+// [] ==> null
+
+function uniqueSum(lst){
+  let ans = [...new Set(lst)].reduce((acc,cur)=>acc+cur,0)
+  return lst.length>0? ans:null
+}
+
+
+
 // Not all integers can be represented by JavaScript/TypeScript. It has space to to represent 53bit signed integers. In this Kata, we've to determine if it is safe to use the integer or not. Make use of the latest ES6 features to find this.
 
 // SafeInteger(9007199254740990) //true
