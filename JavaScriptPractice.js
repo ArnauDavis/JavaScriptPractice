07095fa423248b2512f6a8d1b817b0dc028d868c
ArnauDavis <arnau.davis@outlook.com>
2026-05-14 22:02:28 -0600

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,29 @@
+// Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+// If the array is invalid (empty, or contains negative integers or integers with more than 1 digit), return nil (or your language's equivalent).
+// Examples
+
+// Valid arrays
+
+//     [4, 3, 2, 5] would return [4, 3, 2, 6] (4325 + 1 = 4326)
+//     [1, 2, 3, 9] would return [1, 2, 4, 0] (1239 + 1 = 1240)
+//     [9, 9, 9, 9] would return [1, 0, 0, 0, 0] (9999 + 1 = 10000)
+//     [0, 1, 3, 7] would return [0, 1, 3, 8] (0137 + 1 = 0138)
+
+// Invalid arrays
+
+//     [] is invalid because it is empty
+//     [1, -9] is invalid because -9 is not a non-negative integer
+//     [1, 2, 33] is invalid because 33 is not a single-digit integer
+
+function upArray(arr) {
+  let p1 = arr.length > 0 && arr.every(x => x >= 0 && x <= 9)
+  let ans = p1 ? (BigInt(arr.join('')) + 1n).toString().padStart(arr.length, '0').split('').map(x=>Number(x)) : null
+  return ans
+}
+
+
+
 // The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
 
 // Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
