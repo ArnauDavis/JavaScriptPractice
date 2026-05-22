5e62d86f831c368d25cd8e96e07d77994cd9f53b
ArnauDavis <arnau.davis@outlook.com>
2026-05-22 00:37:22 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,15 @@
+// Write a method that will search an array of strings for all strings that contain another string, ignoring capitalization. Then return an array of the found strings.
+
+// The method takes two parameters, the query string and the array of strings to search, and returns an array.
+
+// If the string isn't contained in any of the strings in the array, the method returns an array containing a single string: "Empty" (or Nothing in Haskell, or "None" in Python and C)
+// Examples
+
+// If the string to search for is "me", and the array to search is ["home", "milk", "Mercury", "fish"], the method should return ["home", "Mercury"].
+
+
+
+
 // Complete the function which will return the area of a circle with the given radius.
 
 // Returned value is expected to be accurate up to tolerance of 0.01.
