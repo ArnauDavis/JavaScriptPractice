925e0276e20f1817104848feb1ea545bdade3339
ArnauDavis <arnau.davis@outlook.com>
2026-06-01 02:45:49 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,23 @@
+// You will be given an array of numbers.
+
+// For each number in the array you will need to create an object.
+
+// The object key will be the number, as a string. The value will be the corresponding character code, as a string.
+
+// Return an array of the resulting objects.
+
+// All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
+
+function numObj(s){
+  let ans = []
+  s.forEach((x)=>{
+    ans.push({[x]:String.fromCharCode(x)})
+  })
+  return ans
+}
+
+
+
 // The function is supposed to check if the input is undefined, but it is not working correctly. Can you fix the code ?
 
 function isUndefined(value) {
