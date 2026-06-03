4b78b393226fc2aed64a475fd1c33ceca76d332f
ArnauDavis <arnau.davis@outlook.com>
2026-06-02 22:18:13 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,24 @@
+// Write a function that takes two string parameters search_text and full_text and returns the number of times the search_text is found within the full_text.
+
+//     Overlap is not permitted: "aaa" contains 1 instance of "aa", not 2.
+//     search_text will never be empty.
+
+// Examples:
+
+// full_text = "aa_bb_cc_dd_bb_e", search_text = "bb"
+// --> should return 2 since "bb" shows up twice
+
+
+// full_text = "aaabbbcccc", search_text = "bbb"
+// --> should return 1
+
+function solution(fullText, search) {
+  let p1 = fullText.split(`${search}`)
+  return p1.length-1
+}
+
+
+
 // You will be given an array of numbers.
 
 // For each number in the array you will need to create an object.
