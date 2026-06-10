5d7baf2dd66e3a086f2f3de7579ad5db18992478
ArnauDavis <arnau.davis@outlook.com>
2026-06-09 23:47:16 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,38 @@
+// Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+
+// Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+
+// Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+
+// Note2: Using a string template can make your job easier.
+
+// Example:
+
+// fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
+// a
+// aa
+// aaa
+// aaaa
+// aaaaa       <---The effect when you console.log it
+// fiveLine("  xy ") 
+// should return "xy\nxyxy\nxyxyxy\nxyxyxyxy\nxyxyxyxyxy"
+// xy
+// xyxy
+// xyxyxy
+// xyxyxyxy
+// xyxyxyxyxy  <---The effect when you console.log it
+
+function fiveLine(s){
+  let p1 = s.trim()
+  let ans = [p1]
+  for(let i=2;i<=5;i++){
+    ans.push(`\n${p1.repeat(i)}`)
+  }
+  return ans.join('')
+}
+
+
+
 // In this kata you are given a string for example:
 
 // "example(unwanted thing)example"
