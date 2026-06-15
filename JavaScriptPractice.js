d67a38b84de51e7625c42ac2150932a93dfd4b62
ArnauDavis <arnau.davis@outlook.com>
2026-06-14 23:54:03 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,49 @@
+// You have to write a calculator that receives strings for input. The dots will represent the number in the equation. There will be dots on one side, an operator, 
+// and dots again after the operator. The dots and the operator will be separated by one space.
+
+// Here are the following valid operators :
+
+//     + Addition
+//     - Subtraction
+//     * Multiplication
+//     // Integer Division
+
+// Your Work (Task)
+
+// You'll have to return a string that contains dots, as many the equation returns. If the result is 0, return the empty string. When it comes to subtraction, 
+// the first number will always be greater than or equal to the second number.
+// Examples (Input => Output)
+
+// * "..... + ..............." => "...................."
+// * "..... - ..."             => ".."
+// * "..... - ."               => "...."
+// * "..... * ..."             => "..............."
+// * "..... * .."              => ".........."
+// * "..... // .."             => ".."
+// * "..... // ."              => "....."
+// * ". // .."                 => ""
+// * ".. - .."                 => ""
+
+function dotCalculator(equation) {
+  const [left, op, right] = equation.split(' ')
+
+  const a = left.length
+  const b = right.length
+
+  switch (op) {
+    case '+':
+      return '.'.repeat(a + b)
+    case '-':
+      return '.'.repeat(a - b)
+    case '*':
+      return '.'.repeat(a * b)
+    case '//':
+      return '.'.repeat(Math.floor(a / b))
+  }
+}
+
+
+
 // Coding in function roundIt. function accept 1 parameter n. It's a number with a decimal point. Please use different methods based on the location of the decimal point, turn the number into an integer.
 
 // If the decimal point is on the left side of the number (that is, the count of digits on the left of the decimal point is less than that on the right), Using ceil() method.
