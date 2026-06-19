a0609af7099953be691237ab9ed4d316d24762f4
ArnauDavis <arnau.davis@outlook.com>
2026-06-18 23:55:43 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,32 @@
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided 2 dimensional array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', 
+// if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+// The sub arrays may not be the same length.
+
+// The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings.
+
+function well(x){
+  let p1 = 0
+  x.forEach((y=>{
+    y.forEach((z)=>{
+     if(z.toString().toLowerCase()=='good'){
+       p1+=1
+     }
+    })
+  }))
+  if(p1===0){
+    return "Fail!"
+  }else if(p1<=2){
+    return "Publish!"
+  }else if(p1>=3){
+    return "I smell a series!"
+  }
+}
+
+
+
 // Complete the function that takes an array of words.
 
 // You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
