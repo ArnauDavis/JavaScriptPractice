9ecf1f7e415e9da49451d3335c245cc888254723
ArnauDavis <arnau.davis@outlook.com>
2026-06-20 23:55:35 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,26 @@
+// Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+// Examples
+
+// (1, 2)     => 1
+// (3, 5)     => 1
+// (-10, 100) => 2
+// (-1, -9)   => 3
+// (19, -56)  => 4
+
+function quadrant(x, y) {
+  if(x>=0&&y>=0){
+    return 1
+  }else if(x<0&&y<0){
+    return 3
+  }else if(x<0&&y>0){
+    return 2
+  }else{
+    return 4
+  }
+}
+
+
+
 // I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), 
 // making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
 
