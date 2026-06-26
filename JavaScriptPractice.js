4413c506a7f89595158cb85eba097c7b31546ee2
ArnauDavis <arnau.davis@outlook.com>
2026-06-25 23:49:28 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,18 @@
+// You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+
+// Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+// Examples
+
+// ["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+// ["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+
+function tailSwap(arr) {
+  let p1 = arr.map((x)=>x.split(':'))
+  return [`${p1[0][0]}:${p1[1][1]}`,`${p1[1][0]}:${p1[0][1]}`]
+}
+
+
+
 // Given an input of an array of objects containing usernames, status and time since last activity (in mins), create a function to work out who is online, offline and away.
 
 // If someone is online but their lastActivity was more than 10 minutes ago they are to be considered away.
