512d9ba446254e583c3abeeb3eece2162136c455
ArnauDavis <arnau.davis@outlook.com>
2026-07-01 22:29:32 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,36 @@
+// Write a simple function to check if the string contains the word hallo in different languages.
+
+// These are the languages of the possible people you met the night before:
+
+//     hello - english
+//     ciao - italian
+//     salut - french
+//     hallo - german
+//     hola - spanish
+//     ahoj - czech republic
+//     czesc - polish
+
+// Notes
+
+//     you can assume the input is a string.
+//     to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
+//     function should be case insensitive to pass the tests
+
+function validateHello(greetings) {
+  let p1 = ['hello','ciao',"salut","hallo","hola","ahoj","czesc"]
+  let p2 = greetings.split(' ')
+  let ans = false
+  for(let i=0;i<p2.length;i++){
+    if(p1.includes(p2[i].toLowerCase().replace(/[^a-zA-Z]/g, ''))){
+    ans=true  
+      break
+    }
+  }
+  return ans
+}
+
+
+
 // An element is leader if it is greater than The Sum all the elements to its right side.
 // Given an array/list [] of integers , Find all the LEADERS in the array.
 // Notes
