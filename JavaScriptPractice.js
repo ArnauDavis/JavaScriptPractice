4857b8f17131e92a4fde13a3379f2e357fe118eb
ArnauDavis <arnau.davis@outlook.com>
2026-07-16 00:35:49 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,25 @@
+// The Stanton measure of an array is defined as follows:
+// Let n be the number of times the value 1 appears in the array.
+// The Stanton measure is then the number of times n appears in the array.
+// Task
+
+// Write a function that takes an integer array and returns its Stanton measure.
+// Examples
+
+//     For [1, 4, 3, 2, 1, 2, 3, 2]:
+//     1 appears 2 times → 2 appears 3 times → Stanton measure = 3.
+
+//     For [1, 4, 1, 2, 11, 2, 3, 1]:
+//     1 appears 3 times → 3 appears 1 time → Stanton measure = 1.
+
+function stantonMeasure(a){
+  let p1 = a.filter((x)=>x===1).length
+  let ans = a.filter((x)=>x===p1).length
+  return ans
+}
+
+
+
 // You will be given a string representing the garden such as:
 
 // garden = 'gravel gravel gravel gravel snail gravel gravel rock gravel slug spider gravel gravel gravel gravel spider gravel rock gravel gravel'
