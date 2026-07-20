32a2204b3b18ef4c0da4c06382afea5b8e661f1c
ArnauDavis <arnau.davis@outlook.com>
2026-07-20 00:01:21 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,25 @@
+// Create a method that takes an array/list as an input, and outputs the index at which the sole odd number is located.
+
+// This method should work with arrays with negative numbers. If there are no odd numbers in the array, then the method should output -1.
+
+// Examples:
+
+// oddOne([2,4,6,7,10]) // => 3
+// oddOne([2,16,98,10,13,78]) // => 4
+// oddOne([4,-8,98,-12,-7,90,100]) // => 4
+// oddOne([2,4,6,8]) // => -1
+
+function oddOne(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      return i
+    }
+  }
+  return -1
+}
+
+
+
 // There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it. 
 // mark the MrFreeze object instance as frozen
 
