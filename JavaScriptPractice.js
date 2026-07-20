6327dbb570b865186182b89714f3126f8c6ada45
ArnauDavis <arnau.davis@outlook.com>
2026-07-19 22:04:41 -0600

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,14 @@
+// i is the imaginary unit, it is defined by i²=−1i² = -1i²=−1, therefore it is a solution to x²+1=0x² + 1 = 0x²+1=0.
+// Your Task
+
+// Complete the function pofi that returns iii to the power of a given non-negative integer in its simplest form, as a string (answer may contain iii).
+
+function pofi(n) {
+  const powers = ["1", "i", "-1", "-i"]
+  return powers[n % 4]
+}
+
+
 
 // Create a method that takes an array/list as an input, and outputs the index at which the sole odd number is located.
 
