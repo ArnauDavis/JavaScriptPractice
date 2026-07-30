7994d410bc224ae3f2a557d1301f6c0695b18219
ArnauDavis <arnau.davis@outlook.com>
2026-07-29 23:32:09 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,26 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450   -> 145
+// 960000 -> 96
+// 1050   -> 105
+// -1050  -> -105
+// 0      -> 0
+
+// Note: Zero should be left as it is.
+
+function noBoringZeros(n) {
+  while(n%10===0&&n!=0){
+    n= n/10
+  }
+  return n
+}
+
+
+
 // Create a function that accepts a string argument and returns an array of strings with each letter from the input string being rotated to the end.
 // Examples:
 
