388596db34b7a82f3894dd926b7439cca70e1387
ArnauDavis <arnau.davis@outlook.com>
2026-07-30 23:58:03 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,28 @@
+// Your task is to return an output string that translates an input string s by replacing each character in s with a number representing the 
+// number of times that character occurs in s and separating each number with the sep character(s).
+
+// Example (s, sep --> Output)
+
+// "hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+// "19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+// "^^^**$"     , "x" --> "3x3x3x2x2x1"
+
+function freqSeq(str, sep) {
+  let p1 = {}
+  for(let i=0;i<str.length;i++){
+    if(!p1.hasOwnProperty(str[i])){
+      p1[str[i]] = 1
+    }else{
+      p1[str[i]]+=1
+    }
+  }
+  let ans = []
+  str.split('').forEach((x)=>ans.push(p1[x]))
+  return ans.join(sep)
+}
+
+
+
 // Numbers ending with zeros are boring.
 
 // They might be fun in your world, but not here.
