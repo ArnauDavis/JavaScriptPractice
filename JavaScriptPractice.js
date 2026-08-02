7698999fa27086754d01d5f1b6ff34af54cd3749
ArnauDavis <arnau.davis@outlook.com>
2026-08-01 23:39:47 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,20 @@
+// Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+// The Circle class can be seen below:
+
+// // Represents a Circle where center is a Point and radius is a Number
+// class Circle {
+//   constructor(center, radius) { 
+//     this.center = center; 
+//     this.radius = radius;
+//   }
+// }
+
+function circleArea(circle){
+  return Math.pow(circle.radius,2)*Math.PI
+}
+
+
+
 // Your task is to return an output string that translates an input string s by replacing each character in s with a number representing the 
 // number of times that character occurs in s and separating each number with the sep character(s).
 
