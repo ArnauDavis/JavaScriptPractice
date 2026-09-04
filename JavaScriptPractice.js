6d70d47f07e9b63de7efa34129390671fa8207df
ArnauDavis <arnau.davis@outlook.com>
2026-09-03 23:55:11 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,38 @@
+// One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and make a password by extracting the first letter of each word.
+
+// Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
+
+//     instead of including i or I put the number 1 in the password;
+//     instead of including o or O put the number 0 in the password;
+//     instead of including s or S put the number 5 in the password.
+
+// Examples:
+
+// "Give me liberty or give me death"  --> "Gml0gmd"
+// "Keep Calm and Carry On"            --> "KCaC0"
+
+function makePassword(phrase) {
+  let p1 = {
+    'i': 1,
+    'I': 1,
+    'o': 0,
+    'O': 0,
+    's': 5,
+    'S': 5,
+  }
+  let ans = []
+  phrase.split(' ').forEach((x)=>{
+    if(Object.keys(p1).includes(x[0])){
+      ans.push(p1[x[0]])
+    }else{
+      ans.push(x[0])
+    }
+  })
+  return ans.join('')
+}
+
+
+
 // you need to write a function that takes a string and a letter as input and then returns the index of the second occurrence of that letter in the string. 
 // If there is no such letter in the string, or if the letter occurs only once in the string, -1 should be returned.
 
