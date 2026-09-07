1357521ce874247feb94b02a781ed0c042e9c305
ArnauDavis <arnau.davis@outlook.com>
2026-09-07 12:33:02 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,24 @@
+// A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value that occurs in more than half of the elements of arr.
+// For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+// The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+// Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns the dominator of arr. 
+// The function should return −1 if array does not have a dominator. All values in arr will be >=0.
+
+function dominator(arr) {
+  const counts = {}
+  for (const item of arr) {
+    counts[item] = (counts[item] || 0) + 1
+  }
+  for (const item in counts) {
+    if (counts[item] > arr.length / 2) {
+      return Number(item);
+    }
+  }
+  return -1
+}
+
+
+
 // There exist two zeroes: +0 (or just 0) and -0.
 
 // Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
