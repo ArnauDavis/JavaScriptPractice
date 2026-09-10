164d5f943d0143f1322f74c94c03ecb0899c8bda
ArnauDavis <arnau.davis@outlook.com>
2026-09-09 23:19:33 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,17 @@
+// Convert a hash into an array. Nothing more, Nothing less.
+
+// {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+
+// should be converted into
+
+// [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+
+function convertHashToArray(hash){
+  return Object.entries(hash)
+}
+
+
+
 // Count the number of occurrences of each character and return them sorted and counted in separate arrays
 
 const orderedCount = function (text) {
