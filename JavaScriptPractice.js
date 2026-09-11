9f803e18f7c58fad8008488cb8b52c6f442eb7c6
ArnauDavis <arnau.davis@outlook.com>
2026-09-11 16:50:23 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,11 @@
+// Write a function that counts how many numbers in an array are even.
+
+function whosOdd(arr){
+	return arr.filter((x)=>x%2==0).length
+}
+
+
+
 // Lot of museum allow you to be a member, for a certain amount amount_by_year you can have unlimitted acces to the museum.
 
 // In this kata you should complete a function in order to know after how many visit it will be better to take an annual pass. The function take 2 arguments annual_price and individual_price. 
