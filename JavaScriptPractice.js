73f693a80834c222888087ea26631188c94b3363
ArnauDavis <arnau.davis@outlook.com>
2026-09-18 13:35:59 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,44 @@
+// Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The files must be saved in the order they appear in the queue.
+
+// Zero size files can always be saved even HD full.
+// Input:
+
+//     Array of file sizes (0 <= s <= 100)
+//     Capacity of the HD (0 <= c <= 500)
+
+// Output:
+
+//     Number of files that can be fully saved in the HD.
+
+// Examples:
+
+// save([4,4,4,3,3], 12) -> 3
+// # 4+4+4 <= 12, but 4+4+4+3 > 12
+
+// save([4,4,4,3,3], 11) -> 2
+// # 4+4 <= 11, but 4+4+4 > 11
+
+// save([12, 0, 0, 1], 12) -> 3
+// # 12+0+0 <= 12, but 12+0+0+1 > 12
+
+// Do not expect any negative or invalid inputs.
+
+function save(sizes, hd) {
+  let count = 0
+  let storage = 0
+  for(let i=0;i<sizes.length;i++){
+    if(storage+sizes[i]<=hd){
+      count+=1
+      storage+=sizes[i]
+    }else{
+      return count
+    }
+  }
+  return count
+}
+
+
+
 // Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. 
 // You should do this without converting the inputted number into a string.
 
