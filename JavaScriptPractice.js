171e199533c0de73dc6f9225b48de81eec560e7d
ArnauDavis <arnau.davis@outlook.com>
2026-09-19 23:36:38 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,14 @@
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+function sortme(names){
+  return names.sort((a,b)=>a.toLowerCase().localeCompare(b.toLowerCase()))
+}
+
+
+
 // Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The files must be saved in the order they appear in the queue.
 
 // Zero size files can always be saved even HD full.
