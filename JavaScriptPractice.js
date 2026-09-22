3157444a50a6026a5c4f697ad7fc9b80d9ea1b84
ArnauDavis <arnau.davis@outlook.com>
2026-09-21 23:48:18 -0400

    added a new solved problem

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,3 +1,26 @@
+// Our loose definition of Vampire Numbers can be described as follows:
+
+// 6 * 21 = 126
+// # 6 and 21 would be valid 'fangs' for a vampire number as the 
+// # digits 6, 1, and 2 are present in both the product and multiplicands
+
+// 10 * 11 = 110
+// # 110 is not a vampire number since there are three 1's in the
+// # multiplicands, but only two 1's in the product
+
+// Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.
+
+function vampireTest(a, b) {
+  if(a<0&&b<0){
+    return false
+  }
+  let p1 = (Math.abs(a*b)).toString().split('').sort().join('')
+  let p2 = (Math.abs(a).toString()+Math.abs(b)).toString().split('').sort().join('')
+  return p1===p2
+}
+
+
+
 // Coding in function howManySmaller, function accept 2 parameter:arr and n. arr is a decimal array. n is a decimal.
 
 // The first mission: let all elements in the array keep two decimal places(No need to convert number n).
